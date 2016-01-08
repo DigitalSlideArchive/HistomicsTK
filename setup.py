bfe5f0394cdd78681be1aba1b23f3bfcdc25ce39
Deepak Roy Chittajallu <deepak.chittajallu@kitware.com>
2016-01-08 16:12:37 -0500

    Populated setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+import setuptools
+import json
+
+with open('README.rst') as f:
+    readme = f.read()
+
+with open('plugin.json') as f:
+    pkginfo = json.load(f)
+
+with open('LICENSE') as f:
+    license = f.read()
+
+setuptools.setup(name='histomicstk',
+                 version=pkginfo['version'],
+                 description=pkginfo['description'],
+                 long_description=readme,
+                 license=license,
+                 url='https://github.com/DigitalSlideArchive/image_analysis',
+                 packages=['histomicstk'])
