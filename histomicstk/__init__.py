7cdfe7ee7bc7e9643db901a86c3a2a2c51a16bf3
Deepak Roy Chittajallu <deepak.chittajallu@kitware.com>
2016-01-15 10:54:21 -0500

    Bump version: 0.1.0 → 0.1.1

--- histomicstk/__init__.py
+++ histomicstk/__init__.py
@@ -12,7 +12,7 @@ from .RudermanLABInv import RudermanLABInv
 from .SparseColorDeconvolution import SparseColorDeconvolution
 from .TilingSchedule import TilingSchedule
 
-__version__ = '0.1.0'
+__version__ = '0.1.1'
 
 __all__ = ['AdaptiveColorNorm',
            'ColorConvolution',
