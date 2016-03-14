281b6d0a158b780404f6c3785ff62e5bc1d19a84
Jonathan Beezley <jonathan.beezley@kitware.com>
2016-03-14 09:42:15 -0400

    Added a modal dialog to select an available spec

--- web_client/js/views/body.js
+++ web_client/js/views/body.js
@@ -6,11 +6,17 @@ histomicstk.views.Body = girder.View.extend({
         this.panelGroupView = new histomicstk.views.PanelGroup({
             parentView: this
         });
-
+        this.listenTo(histomicstk.router, 'route:main', this.selectGui);
     },
     render: function () {
         this.$el.html(histomicstk.templates.body());
         this.visView.setElement(this.$('#h-vis-container')).render();
         this.panelGroupView.setElement(this.$('#h-panel-group-container')).render();
+    },
+    selectGui: function () {
+        new histomicstk.views.GuiSelectorWidget({
+            el: $('#g-dialog-container'),
+            parentView: this
+        }).render();
     }
 });
