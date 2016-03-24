714b9749912160f27cac57007e94c79bc8c9a39d
Jonathan Beezley <jonathan.beezley@kitware.com>
2016-03-24 09:19:16 -0400

    Render a geojs visualization... with cats!

--- web_client/js/views/visualization.js
+++ web_client/js/views/visualization.js
@@ -1,18 +1,32 @@
 histomicstk.views.Visualization = girder.View.extend({
     initialize: function () {
-        this.render();
-        this._onresize = _.bind(this.render, this);
-        $(window).on('resize', this._onresize);
+        this._map = geo.map({
+            node: '<div style="width: 100%; height: 100%"/>',
+            width: 100,
+            height: 100
+        });
+        this._map.createLayer('osm', {
+            url: function () { return 'http://lorempixel.com/256/256/cats/'; },
+            keepLower: false,
+            wrapX: true,
+            wrapY: true,
+            attribution: 'Images provided by <a href="http://lorempixel.com">lorempixel</a>'
+        });
     },
-    render: _.debounce(function () {
-        var width = this.$el.width(),
-            height = this.$el.height();
-        this.$el.html(histomicstk.templates.visualization({
-            url: 'http://lorempixel.com/' + width + '/' + height + '/cats/'
-        }));
-    }, 500),
+    render: function () {
+        var mapnode = this._map.node();
+        if (this.$el) {
+            this.$el.empty();
+            this.$el.append(mapnode);
+            this._map.size({
+                width: this.$el.width(),
+                height: this.$el.height()
+            });
+        }
+        return this;
+    },
     destroy: function () {
-        $(window).off('resize', this._onresize);
+        this._map.exit();
         girder.View.prototype.destroy.apply(this, arguments);
     }
 });
