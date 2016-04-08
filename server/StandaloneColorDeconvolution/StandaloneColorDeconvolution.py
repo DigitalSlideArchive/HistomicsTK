67828651c0fc7db9c1de6adf8535ce3fa3b3a195
Deepak Roy Chittajallu <deepak.chittajallu@kitware.com>
2016-04-08 17:20:34 -0400

    Added a standalone CLI to test

---
+++ server/StandaloneColorDeconvolution/StandaloneColorDeconvolution.py
@@ -0,0 +1,44 @@
+from ctk_cli import CLIArgumentParser
+import logging
+logging.basicConfig()
+
+import numpy as np
+import skimage.io
+import histomicstk as htk
+
+
+def main(args):
+
+    # Read Input Image
+    print('>> Reading input image')
+
+    print(args.inputImageFile)
+
+    inputImage = skimage.io.imread(args.inputImageFile)
+
+    # Create stain matrix
+    print('>> Creating stain matrix')
+
+    W = np.array([args.stainColor_1, args.stainColor_2, args.stainColor_3]).T
+    print W
+
+    # Perform color deconvolution
+    print('>> Performing color deconvolution')
+    res = htk.ColorDeconvolution(inputImage, W)
+
+    # write stain images to output
+    print('>> Outputting individual stain images')
+
+    print args.outputStainImageFile_1
+    skimage.io.imsave(args.outputStainImageFile_1, res.Stains[:, :, 0])
+
+    print args.outputStainImageFile_2
+    skimage.io.imsave(args.outputStainImageFile_2, res.Stains[:, :, 1])
+
+    print args.outputStainImageFile_3
+    skimage.io.imsave(args.outputStainImageFile_3, res.Stains[:, :, 2])
+
+
+if __name__ == "__main__":
+    main(CLIArgumentParser().parse_args())
+
