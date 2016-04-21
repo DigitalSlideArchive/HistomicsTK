c77af43037d0da97337ab9e8f32acaeb8bbf6e16
Lee Cooper <lee.cooper@emory.edu>
2016-04-21 15:29:54 +0200

    Update __init__.py
    Added functions 'FilterLabel' and 'MaxClustering'.

--- histomicstk/__init__.py
+++ histomicstk/__init__.py
@@ -11,12 +11,14 @@ from .Del2 import Del2
 from .DregEdge import DregEdge
 from .EmbedBounds import EmbedBounds
 from .EstimateVariance import EstimateVariance
+from .FilterLabel import FilterLabel
 from .GaussianGradient import GaussianGradient
 from .GaussianVoting import GaussianVoting
 from .gLoG import gLoG
 from .GradientDiffusion import GradientDiffusion
 from .GradientFlow import GradientFlow
 from .LabelPerimeter import LabelPerimeter
+from .MaxClustering import MaxClustering
 from .MergeSinks import MergeSinks
 from .OpticalDensityFwd import OpticalDensityFwd
 from .OpticalDensityInv import OpticalDensityInv
