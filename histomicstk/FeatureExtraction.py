08a3e605b4dc4f6428ba0d2f80b066832b6491c8
slee172 <shlee8020@gmail.com>
2016-05-01 17:47:00 -0400

    Update FeatureExtraction.py added GradientFeatureGroup function

--- histomicstk/FeatureExtraction.py
+++ histomicstk/FeatureExtraction.py
@@ -288,8 +288,8 @@ def GradientFeatureGroup(I, Coords):
         ]
     )
     Output = iFG(
-        MeanGradMag, StdGradMag, EntropyGradMag, EnergyGradMag
-        , SkewnessGradMag, KurtosisGradMag, SumCanny, MeanCanny
+        MeanGradMag, StdGradMag, EntropyGradMag, EnergyGradMag,
+        SkewnessGradMag, KurtosisGradMag, SumCanny, MeanCanny
     )
 
     return Output
@@ -360,8 +360,8 @@ def IntensityFeatureGroup(I, Coords):
         ]
     )
     Output = iFG(
-        MeanIntensity, MeanMedianDifferenceIntensity
-        , MaxIntensity, MinIntensity, StdIntensity
+        MeanIntensity, MeanMedianDifferenceIntensity,
+        MaxIntensity, MinIntensity, StdIntensity
     )
 
     return Output
