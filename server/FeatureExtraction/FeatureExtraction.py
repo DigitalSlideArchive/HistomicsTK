d18b39d9670c550319401f0d439a8ae02cf7b5c9
Sanghoon Lee <shlee8020@gmail.com>
2016-05-25 11:43:46 -0400

    Updated FeatureExtraction.py removed hard-coded parameters and followed NucleiSegmentation processing

--- server/FeatureExtraction/FeatureExtraction.py
+++ server/FeatureExtraction/FeatureExtraction.py
@@ -1,53 +1,105 @@
+from ctk_cli import CLIArgumentParser
+import histomicstk as htk
 import numpy as np
-import pandas
-import scipy
+import pandas as pd
+import scipy as sp
 import skimage.io
 import skimage.morphology
-
-import histomicstk as htk
-from ctk_cli import CLIArgumentParser
 
 import logging
 logging.basicConfig()
 
+stainColorMap = {
+    'hematoxylin': [0.65, 0.70, 0.29],
+    'eosin':       [0.07, 0.99, 0.11],
+    'dab':         [0.27, 0.57, 0.78],
+    'null':        [0.0, 0.0, 0.0]
+}
 
 def main(args):
 
+    #
     # Read Input Image
+    #
     print('>> Reading input image')
 
-    print '--- Image Name = ', args.inputImageFile
+    imInput = skimage.io.imread(args.inputImageFile)[:, :, :3]
 
-    Standard = skimage.io.imread(args.inputImageFile)[:, :, :3]
-    print '--- Original Image Size = ', Standard.shape
+    #
+    # Perform color normalization
+    #
+    print('>> Performing color normalization')
 
-    # Create stain matrix
-    print('>> Creating stain matrix')
-    W = np.array([args.stainColor_1, args.stainColor_2, args.stainColor_3]).T
+    # transform input image to LAB color space
+    imInputLAB = htk.RudermanLABFwd(imInput)
 
+    # compute mean and stddev of input in LAB color space
+    Mu = np.zeros(3)
+    Sigma = np.zeros(3)
+
+    for i in range(3):
+        Mu[i] = imInputLAB[:, :, i].mean()
+        Sigma[i] = (imInputLAB[:, :, i] - Mu[i]).std()
+
+    # perform reinhard normalization
+    imNmzd = htk.ReinhardNorm(imInput, Mu, Sigma)
+
+    #
     # Perform color deconvolution
+    #
     print('>> Performing color deconvolution')
-    UNS = htk.ColorDeconvolution(Standard, W)
 
-    # Constrained log filtering=== - generate R_{N}(x,y)
-    Nuclei = UNS.Stains[::2, ::2, 0].astype(dtype=np.float)
-    Mask = scipy.ndimage.morphology.binary_fill_holes(Nuclei < 160)
+    stainColor_1 = stainColorMap[args.stain_1]
+    stainColor_2 = stainColorMap[args.stain_2]
+    stainColor_3 = stainColorMap[args.stain_3]
 
-    Response = htk.cLoG(Nuclei, Mask, SigmaMin=4*1.414, SigmaMax=7*1.414)
-    Label, Seeds, Max = htk.MaxClustering(Response.copy(), Mask, 10)
-    Filtered = htk.FilterLabel(Label, 4, 80, None).astype(dtype=np.int)
+    W = np.array([stainColor_1, stainColor_2, stainColor_3]).T
 
-    # Get Hematoxylin and Eosin
-    Hematoxylin = UNS.Stains[::2, ::2, 0]
-    Eosin = UNS.Stains[::2, ::2, 1]
+    imDeconvolved = htk.ColorDeconvolution(imNmzd, W)
 
+    imNucleiStain = imDeconvolved.Stains[::2, ::2, 0].astype(np.float)
+
+    #
+    # Perform nuclei segmentation
+    #
+    print('>> Performing nuclei segmentation')
+
+    # segment foreground
+    imFgndMask = sp.ndimage.morphology.binary_fill_holes(
+        imNucleiStain < args.foreground_threshold)
+
+    # run adaptive multi-scale LoG filter
+    imLog = htk.cLoG(imNucleiStain, imFgndMask,
+                     SigmaMin=args.min_radius * np.sqrt(2),
+                     SigmaMax=args.max_radius * np.sqrt(2))
+
+    imNucleiSegMask, Seeds, Max = htk.MaxClustering(
+        imLog, imFgndMask, args.local_max_search_radius)
+
+    # filter out small objects
+    imNucleiSegMask = htk.FilterLabel(
+        imNucleiSegMask, Lower=args.min_nucleus_area).astype(np.int)
+
+    # get Hematoxylin and Eosin
+    Hematoxylin = imDeconvolved.Stains[::2, ::2, 0]
+    Eosin = imDeconvolved.Stains[::2, ::2, 1]
+
+    #
     # Perform feature extraction
+    #
     print('>> Performing feature extraction')
-    df = htk.FeatureExtraction(Filtered, Hematoxylin, Eosin, W)
 
+    df = htk.FeatureExtraction(
+        imNucleiSegMask, Hematoxylin, Eosin, W, args.K, args.Fs, args.Delta)
+
+    #
+    # Create HDF5 file
+    #
     print('>> Writing HDF5 file')
-    hdf = pandas.HDFStore(args.outputFile)
+
+    hdf = pd.HDFStore(args.outputFile)
     hdf.put('d1', df, format='table', data_columns=True)
+
     print '--- Object x Features = ', hdf['d1'].shape
 
 
