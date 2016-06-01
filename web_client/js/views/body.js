57d089d54aec4a3ddf4f4c59b642c7ab2294ea0c
Jonathan Beezley <jonathan.beezley@kitware.com>
2016-06-01 16:07:26 -0400

    Replace gui selector modal with a dropdown

--- web_client/js/views/body.js
+++ web_client/js/views/body.js
@@ -6,17 +6,10 @@ histomicstk.views.Body = girder.View.extend({
         this.panelGroupView = new histomicstk.views.PanelGroup({
             parentView: this
         });
-        this.listenTo(histomicstk.router, 'route:main', this.selectGui);
     },
     render: function () {
         this.$el.html(histomicstk.templates.body());
         this.visView.setElement(this.$('#h-vis-container')).render();
         this.panelGroupView.setElement(this.$('#h-panel-controls-container')).render();
-    },
-    selectGui: function () {
-        new histomicstk.views.GuiSelectorWidget({
-            el: $('#g-dialog-container'),
-            parentView: this
-        }).render();
     }
 });
--- web_client/js/views/header.js
+++ web_client/js/views/header.js
@@ -1,8 +1,24 @@
 histomicstk.views.Header = girder.views.LayoutHeaderUserView.extend({
+    events: {
+        'click #g-analysis-menu a': '_selectAnalysis'
+    },
+    initialize: function () {
+        this.analyses = [];
+        girder.restRequest({path: 'HistomicsTK'})
+            .then(_.bind(function (data) {
+                this.analyses = data;
+                this.render();
+            }, this));
+    },
     render: function () {
         this.$el.html(histomicstk.templates.header({
-            user: girder.currentUser
+            user: girder.currentUser,
+            analyses: this.analyses
         }));
         return this;
+    },
+    _selectAnalysis: function (evt) {
+        var name = $(evt.currentTarget).data('name');
+        histomicstk.router.navigate(name, {trigger: true});
     }
 });
