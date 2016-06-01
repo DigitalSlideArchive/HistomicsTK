e623dbcf142c3f3aa77916947d369b47fd5736d3
Sanghoon Lee <shlee8020@gmail.com>
2016-06-01 11:17:19 -0400

    Updated FeatureExtractionHaralick.py added the rest of features

--- histomicstk/FeatureExtractionHaralick.py
+++ histomicstk/FeatureExtractionHaralick.py
@@ -8,7 +8,7 @@ def FeatureExtractionHaralick(I, Dst=1, Ng=256):
     Parameters
     ----------
     I : array_like
-        An RGB image where in each channel contains image of one stain
+        An intensity image
     Dst : Distance of neighboring pixels in an image. Default value = 1.
     Ng : Number of gray level co-occurance matrix. Default value = 256.
     Returns
@@ -41,43 +41,62 @@ def FeatureExtractionHaralick(I, Dst=1, Ng=256):
         p_ij = P_ij[:, :, 0, r]/R
         # gets marginal-probability matrix sets summing the rows of p_ij
         px = np.sum(p_ij, axis=1)
+        py = np.sum(p_ij, axis=0)
         # initialize marginal-probability matrix sets summing p_ij
         # that i+j = k or i-j = k
         pxPlusy = np.zeros(2*Ng-1)
         pxMinusy = np.zeros(Ng)
         # arbitarily small positive constant
-        # e = 0.00001
+        e = 0.00001
         for i in range(0, Ng):
             for j in range(0, Ng):
                 # gets marginal-probability matrix
                 pxPlusy[i+j] = pxPlusy[i+j] + p_ij[i, j]
                 pxMinusy[abs(i-j)] = pxMinusy[abs(i-j)] + p_ij[i, j]
         # f0: computes angular second moment
-        f[0, r] = np.sum(p_ij**2)
+        f[0, r] = np.sum(np.square(p_ij))
         # f1: computes constast
         n_Minus = np.arange(Ng)
-        f[1, r] = np.dot(n_Minus**2, pxMinusy)
+        f[1, r] = np.dot(np.square(n_Minus), pxMinusy)
         # f2: computes correlation
         # gets weighted mean and weighted standard deviation of px and py
         meanx = np.dot(n_Minus, px)
-        variance = np.dot(px, n_Minus**2) - meanx**2
+        variance = np.dot(px, np.square(n_Minus)) - np.square(meanx)
         p_ijr = np.ravel(p_ij)
         i, j = np.mgrid[0:Ng, 0:Ng]
         ij = i*j
-        f[2, r] = (np.dot(np.ravel(ij), p_ijr) - meanx**2) / variance
+        f[2, r] = (np.dot(np.ravel(ij), p_ijr) - np.square(meanx)) / variance
         # f3: computes sum of squares : variance
         f[3, r] = variance
         # f4: computes inverse difference moment
-        ij_IDM = 1. / (1+(i-j)**2)
+        ij_IDM = 1. / (1+np.square(i-j))
         f[4, r] = np.dot(np.ravel(ij_IDM), p_ijr)
-        f[5, r] = 0
-        f[6, r] = 0
-        f[7, r] = 0
-        f[8, r] = 0
-        f[9, r] = 0
-        f[10, r] = 0
-        f[11, r] = 0
-        f[12, r] = 0
+        # f5: computes sum average
+        n_Plus = np.arange(2*Ng-1)
+        f[5, r] =  np.dot(n_Plus, pxPlusy)
+        # f6: computes sum variance
+        # [1] uses sum entropy, but we use sum average
+        f[6, r] = np.dot(n_Plus**2, pxPlusy) - np.square(f[5, r])
+        # f7: computes sum entropy
+        f[7, r] = -np.dot(pxPlusy, np.log2(pxPlusy+e))
+        # f8: computes entropy
+        f[8, r] = -np.dot(p_ijr, np.log2(p_ijr+e))
+        # f9: computes variance px-y
+        f[9, r] = np.var(pxMinusy)
+        # f10: computes difference entropy px-y
+        f[10, r] = -np.dot(pxMinusy, np.log2(pxMinusy+e))
+        # f11: computes information measures of correlation
+        # gets entropies of px and py
+        HX = -np.dot(px, np.log2(px+e))
+        HY = -np.dot(py, np.log2(py+e))
+        HXY = f[8, r]
+        pxy_ij = np.outer(px, py)
+        pxy_ijr = np.ravel(pxy_ij)
+        HXY1 = -np.dot(p_ijr, np.log2(pxy_ijr+e))
+        HXY2 =  -np.dot(pxy_ijr, np.log2(pxy_ijr+e))
+        f[11, r] =  (HXY-HXY1)/max(HX, HY)
+        # f12: computes information measures of correlation
+        f[12, r] =  np.sqrt(1 - np.exp(-2.0*(HXY2-HXY)))
     # computes means and ranges of the 13 features with 4 angles
     # and store it to H
     H = np.zeros(26)
