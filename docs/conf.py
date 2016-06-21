901d238c511412fd536fcd982f1eb2615779aa0d
Deepak Roy Chittajallu <deepak.chittajallu@kitware.com>
2016-06-21 17:52:20 -0400

    Add nbsphinx to sphinx extensions

--- docs/conf.py
+++ docs/conf.py
@@ -97,7 +97,7 @@ release = version
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
-exclude_patterns = ['_build']
+exclude_patterns = ['_build', '**.ipynb_checkpoints']
 
 # The reST default role (used for this markup: `text`) to use for all
 # documents.
