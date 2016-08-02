4d9b632220c5299604ff13a7739d385759f85b7b
Sanghoon Lee <shlee8020@gmail.com>
2016-08-02 00:37:48 -0400

    Updated ComputeIntensityFeatures.py

--- histomicstk/features/ComputeIntensityFeatures.py
+++ histomicstk/features/ComputeIntensityFeatures.py
@@ -1,4 +1,5 @@
 import numpy as np
+from scipy.stats import entropy, skew, kurtosis
 
 def ComputeIntensityFeatures(I, Coords):
     """
@@ -13,26 +14,44 @@ def ComputeIntensityFeatures(I, Coords):
 
     Returns
     -------
-    MeanIntensity : float
-        Mean of intensity data.
-    MeanMedianDifferenceIntensity : float
-        Difference between mean and median.
-    MaxIntensity : float
-        Max intensity data.
-    MinIntensity : float
-        Min intensity data.
-    StdIntensity : float
-        Standard deviation of intensity data.
+    IGroup : array_like
+        A 1 x 9 intensity features.
+
+    - `Intensity features`:
+    MeanIntensity : Mean of intensity data.
+    MeanMedianDifferenceIntensity : Difference between mean and median.
+    MaxIntensity : Max intensity data.
+    MinIntensity : Min intensity data.
+    StdIntensity : Standard deviation of intensity data.
+    Entropy : Entroy of intensity data.
+    Energy : Energy of intensity data.
+    Skewness : Skewness of intensity data.
+        Value is 0 when all values are equal.
+    Kurtosis : Kurtosis of intensity data.
+        Value is -3 when all values are equal.
     """
 
+    IGroup = np.zeros(9)
     pixOfInterest = I[Coords[:, 0], Coords[:, 1]]
-    # compute intensity features
-    MeanIntensity = np.mean(pixOfInterest)
-    MeanMedianDifferenceIntensity = MeanIntensity - \
-        np.median(pixOfInterest)
-    MaxIntensity = max(pixOfInterest)
-    MinIntensity = min(pixOfInterest)
-    StdIntensity = np.std(pixOfInterest)
+    # compute mean
+    IGroup[0] = np.mean(pixOfInterest)
+    # compute mean median differnece
+    IGroup[1] = IGroup[0] - np.median(pixOfInterest)
+    # compute max
+    IGroup[2] = max(pixOfInterest)
+    # compute min
+    IGroup[3] = min(pixOfInterest)
+    # compute standard
+    IGroup[4] = np.std(pixOfInterest)
+    # compute entropy
+    IGroup[5] = entropy(pixOfInterest)
+    # compute energy
+    hist, bins = np.histogram(pixOfInterest, bins=np.arange(256))
+    prob = hist/np.sum(hist, dtype=np.float32)
+    IGroup[6] = np.sum(np.power(prob, 2))
+    # compute skewness
+    IGroup[7] = skew(pixOfInterest)
+    # compute kurtosis
+    IGroup[8] = kurtosis(pixOfInterest)
 
-    return MeanIntensity, MeanMedianDifferenceIntensity, \
-        MaxIntensity, MinIntensity, StdIntensity
+    return IGroup
