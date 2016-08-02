db1fe0cf04d64ea59e58a4e679343aaed00fc203
Sanghoon Lee <shlee8020@gmail.com>
2016-08-01 22:34:52 -0400

    Update __init__.py - ComputeFSDs.py, ComputeGradientFeatures.py, ComputeIntensityFeatures.py, ComputeTextureFeatures.py

--- histomicstk/features/__init__.py
+++ histomicstk/features/__init__.py
@@ -1,10 +1,18 @@
 # make functions available at the package level using shadow imports
 # since we mostly have one function per file
+from .ComputeFSDs import ComputeFSDs
+from .ComputeGradientFeatures import ComputeGradientFeatures
+from .ComputeIntensityFeatures import ComputeIntensityFeatures
+from .ComputeTextureFeatures import ComputeTextureFeatures
 from .FeatureExtraction import FeatureExtraction
 
 # list out things that are available for public use
 __all__ = (
 
     # functions and classes of this package
+    'ComputeFSDs',
+    'ComputeGradientFeatures',
+    'ComputeIntensityFeatures',
+    'ComputeTextureFeatures',
     'FeatureExtraction',
 )
