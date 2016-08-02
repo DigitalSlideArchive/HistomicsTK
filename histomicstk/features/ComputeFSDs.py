e96db708cec56367a5674479ba37a04553999ae6
Sanghoon Lee <shlee8020@gmail.com>
2016-08-02 19:01:40 -0400

    Refactored FSDs

--- histomicstk/features/ComputeFSDs.py
+++ histomicstk/features/ComputeFSDs.py
@@ -1,24 +1,33 @@
 import collections
 import numpy as np
+import pandas as pd
+from skimage.measure import regionprops
 from skimage.segmentation import find_boundaries
 
 
-def ComputeFSDs(Label, K=128, Fs=6):
+def ComputeFSDs(im_label, K=128, Fs=6, Delta=8):
     """
-    Calculates `Fourier shape descriptors` from a label mask.
+    Calculates `Fourier shape descriptors` for each objects.
 
     Parameters
     ----------
-    Label : array_like
-        A M x N label mask.
-    K : Number of points for boundary resampling to calculate fourier
+    im_label : array_like
+        A labeled mask image wherein intensity of a pixel is the ID of the
+        object it belongs to. Non-zero values are considered to be foreground
+        objects.
+    K : int
+        Number of points for boundary resampling to calculate fourier
         descriptors. Default value = 128.
-    Fs : Number of frequency bins for calculating FSDs. Default value = 6.
+    Fs : int
+        Number of frequency bins for calculating FSDs. Default value = 6.
+    Delta : int
+        Used to dilate nuclei and define cytoplasm region.
+        Default value = 8.
 
     Returns
     -------
-    FSDGroup : array_like
-        A 1 x Fs FSD arrays.
+    fdata: Pandas data frame containing the FSD features for each
+           object/label.
 
     References
     ----------
@@ -26,14 +35,22 @@ def ComputeFSDs(Label, K=128, Fs=6):
        Fourier descriptors with different shape signatures," In Proc.
        ICIMADE01, 2001.
     """
+    # List of feature names
+    feature_list = []
+    for i in range(0, Fs):
+        feature_list = np.append(feature_list, 'FSD' + str(i+1))
 
-    # initialize FSD feature group
-    FSDGroup = np.zeros(Fs)
+    # get Label size x
+    sizex = im_label.shape[0]
+    sizey = im_label.shape[1]
 
-    # find nucleus boundaries
-    Bounds = np.argwhere(
-        find_boundaries(Label, mode="inner").astype(np.uint8) == 1
-    )
+    # get the number of objects in Label
+    rprops = regionprops(im_label)
+
+    numFeatures = len(feature_list)
+    numLabels = len(rprops)
+    fdata = pd.DataFrame(np.zeros((numLabels, numFeatures)),
+                         columns=feature_list)
 
     # fourier descriptors, spaced evenly over the interval 1:K/2
     Interval = np.round(
@@ -42,12 +59,25 @@ def ComputeFSDs(Label, K=128, Fs=6):
         )
     ).astype(np.uint8)
 
-    FSDGroup = FSDs(Bounds[:, 0], Bounds[:, 1], K, Interval)
+    for i in range(numLabels):
+        # get bounds of dilated nucleus
+        min_row, max_row, min_col, max_col = \
+            _GetBounds(rprops[i].bbox, Delta, sizex, sizey)
+        # grab label mask
+        lmask = (
+            im_label[min_row:max_row, min_col:max_col] == rprops[i].label
+        ).astype(np.bool)
+        # find boundaries
+        Bounds = np.argwhere(
+            find_boundaries(lmask, mode="inner").astype(np.uint8) == 1
+        )
+        # compute fourier descriptors
+        fdata.at[i, :] = _FSDs(Bounds[:, 0], Bounds[:, 1], K, Interval)
 
-    return FSDGroup
+    return fdata
 
 
-def InterpolateArcLength(X, Y, L):
+def _InterpolateArcLength(X, Y, L):
     """
     Resamples boundary points [X, Y] at L total equal arc-length locations.
 
@@ -116,11 +146,11 @@ def InterpolateArcLength(X, Y, L):
     return Output
 
 
-def FSDs(X, Y, K, Intervals):
+def _FSDs(X, Y, K, Intervals):
     """
-    Calculated FSDs from boundary points X,Y. Boundaries are resampled to
-    have K equally spaced points (arclength) around the shape. The curvature
-    is calculated using the cumulative angular function, measuring the
+    Calculated FSDs from boundary points X,Y. Boundaries are resampled to have
+    K equally spaced points (arclength) around the shape. The curvature is
+    calculated using the cumulative angular function, measuring the
     displacement of the tangent angle from the starting point of the boundary.
     The K-length fft of the cumulative angular function is calculated, and
     then the elements of 'F' are summed as the spectral energy over
@@ -155,7 +185,7 @@ def FSDs(X, Y, K, Intervals):
     # initialize F
     F = np.zeros((L-1, )).astype(float)
     # interpolate boundaries
-    iXY = InterpolateArcLength(X, Y, K)
+    iXY = _InterpolateArcLength(X, Y, K)
     # check if iXY.iX is not empty
     if iXY.iX.size:
         # calculate curvature
@@ -177,3 +207,41 @@ def FSDs(X, Y, K, Intervals):
             ).real.astype(float)
 
     return F
+
+
+def _GetBounds(bbox, delta, M, N):
+    """
+    Returns bounds of object in global label image.
+
+    Parameters
+    ----------
+    bbox : tuple
+        Bounding box (min_row, min_col, max_row, max_col).
+    delta : int
+        Used to dilate nuclei and define cytoplasm region.
+        Default value = 8.
+    M : int
+        X size of label image.
+    N : int
+        Y size of label image.
+
+    Returns
+    -------
+    min_row : int
+        Minum row of the region bounds.
+    max_row : int
+        Maximum row of the region bounds.
+    min_col : int
+        Minum column of the region bounds.
+    max_col : int
+        Maximum column of the region bounds.
+    """
+
+    min_row, min_col, max_row, max_col = bbox
+
+    min_row_out = max(0, (min_row - delta))
+    max_row_out = min(M-1, (max_row + delta))
+    min_col_out = max(0, (min_col - delta))
+    max_col_out = min(N-1, (max_col + delta))
+
+    return min_row_out, max_row_out, min_col_out, max_col_out
