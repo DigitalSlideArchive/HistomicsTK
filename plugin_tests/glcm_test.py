ef4e8500e84aca2f051d452c243d9fea7f775dc4
Deepak Roy Chittajallu <deepak.chittajallu@kitware.com>
2016-08-05 18:54:07 -0400

    Add glcm_test.py adn fix bugs

---
+++ plugin_tests/glcm_test.py
@@ -0,0 +1,105 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+###############################################################################
+#  Copyright Kitware Inc.
+#
+#  Licensed under the Apache License, Version 2.0 ( the "License" );
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+###############################################################################
+
+from tests import base
+
+import histomicstk as htk
+import numpy as np
+import os
+import skimage.feature
+
+
+# boiler plate to start and stop the server if needed
+def setUpModule():
+    base.enabledPlugins.append('HistomicsTK')
+    base.startServer()
+
+
+def tearDownModule():
+    base.stopServer()
+
+
+TEST_DATA_DIR = os.path.join(os.environ['GIRDER_TEST_DATA_PREFIX'],
+                             'plugins/HistomicsTK')
+
+
+class GLCMMatrixGenerationTest(base.TestCase):
+
+    def test_graycomatrixext(self):
+
+        image = np.array([[0, 0, 1, 1],
+                          [0, 0, 1, 1],
+                          [0, 2, 2, 2],
+                          [2, 2, 3, 3]], dtype=np.uint8)
+
+        # test 0 degrees or [0, 1]
+        res_skim = skimage.feature.greycomatrix(
+            image,[1],
+            [0],
+            levels=4
+        )
+
+        res_htk = htk.features.graycomatrixext(
+            image, num_levels=4, gray_limits=[0, 3],
+            offsets=np.array([[0,1])]
+        )
+
+        self.assertEqual(res_skim, res_htk)
+
+        # test 90 degrees or [1, 0]
+        res_skim = skimage.feature.greycomatrix(
+            image,[1],
+            [np.pi/2],
+            levels=4
+        )
+
+        res_htk = htk.features.graycomatrixext(
+            image, num_levels=4, gray_limits=[0, 3],
+            offsets=np.array([[1,0])]
+        )
+
+        self.assertEqual(res_skim, res_htk)
+
+        # test 45 degrees or [1, 1]
+        res_skim = skimage.feature.greycomatrix(
+            image,[1],
+            [np.pi/4],
+            levels=4
+        )
+
+        res_htk = htk.features.graycomatrixext(
+            image, num_levels=4, gray_limits=[0, 3],
+            offsets=np.array([[1,1]])
+        )
+
+        self.assertEqual(res_skim, res_htk)
+
+        # test 135 degrees or [1, -1]
+        res_skim = skimage.feature.greycomatrix(
+            image,[1],
+            [3 * np.pi/4],
+            levels=4
+        )
+
+        res_htk = htk.features.graycomatrixext(
+            image, num_levels=4, gray_limits=[0, 3],
+            offsets=np.array([[1,-1]])
+        )
+
+        self.assertEqual(res_skim, res_htk)
