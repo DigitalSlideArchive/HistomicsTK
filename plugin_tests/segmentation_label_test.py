310ac7b07cc873f382ea6edf43ecba4f63bf1c95
Sanghoon Lee <shlee8020@gmail.com>
2016-09-12 20:49:06 -0400

    Fix double space error in segmentation_label_test.py

--- plugin_tests/segmentation_label_test.py
+++ plugin_tests/segmentation_label_test.py
@@ -24,6 +24,7 @@ import os
 
 from histomicstk.segmentation import label
 
+
 # boiler plate to start and stop the server if needed
 def setUpModule():
     base.enabledPlugins.append('HistomicsTK')
