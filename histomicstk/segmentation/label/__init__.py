cc69ba5f925e9b754d5469c68f0e067af89ba303
Sanghoon Lee <shlee8020@gmail.com>
2016-09-20 22:17:16 -0400

    Added tracebounds_cython to __init__.py

--- histomicstk/segmentation/label/__init__.py
+++ histomicstk/segmentation/label/__init__.py
@@ -12,6 +12,7 @@ from .DeleteLabel import DeleteLabel
 from .LabelPerimeter import LabelPerimeter
 from .ShuffleLabel import ShuffleLabel
 from .TraceBounds import TraceBounds
+from .tracebounds_cython import tracebounds_cython
 
 # must be imported after CondenseLabel
 from .AreaOpenLabel import AreaOpenLabel
