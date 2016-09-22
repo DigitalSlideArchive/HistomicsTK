e2397028d1e887fd6aa69bc7c4409c3a727a2639
Sanghoon Lee <shlee8020@gmail.com>
2016-09-22 10:55:17 -0400

    Updated isbfcpp.h with std

--- histomicstk/segmentation/label/isbfcpp.h
+++ histomicstk/segmentation/label/isbfcpp.h
@@ -5,37 +5,15 @@ C++ version of ISBF for TraceBounds
 #define isbfcpp_h
 
 #include <iostream>
-#include <string>
 #include <vector>
-
-struct node
-{
-    int val_x;
-    int val_y;
-    node *next;
-};
 
 class isbfcpp
 {
-private:
-    node *head;
-    node *curr;
-    int count;
-    int nrows;
-    int ncols;
-    int** matrix00;
-    int** matrix90;
-    int** matrix180;
-    int** matrix270;
 
 public:
     isbfcpp();
-    int length();
-    void nth_from_last(int n, int &x, int &y);
-    bool addList(int x, int y);
-    void rotateMatrix(int rows, int cols, int **input, int **output);
-    std::vector<int> getList(int nrows, int ncols, int *size, int *mask, int startX, int startY, float inf);
-    void clean();
+    std::vector <std::vector<int> > rotateMatrix(int rows, int cols,  std::vector <std::vector<int> > input);
+    std::vector <std::vector<int> > traceBoundary(int nrows, int ncols, std::vector <std::vector<int> > mask, int startX, int startY, float inf);
     ~isbfcpp();
 };
 
