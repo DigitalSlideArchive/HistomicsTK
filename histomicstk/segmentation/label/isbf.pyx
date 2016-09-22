2bdedd64e237fe7db1a8250f717964586ec80971
Sanghoon Lee <shlee8020@gmail.com>
2016-09-22 10:54:47 -0400

    Updated isbf.pyx with std

--- histomicstk/segmentation/label/isbf.pyx
+++ histomicstk/segmentation/label/isbf.pyx
@@ -1,12 +1,11 @@
 """
-Cython source code: ISBF for TraceBounds
+Cython source code: ISBF for trace_boundary
 """
 import cython
 
 import numpy as np
 cimport numpy as np
 
-from libcpp cimport bool
 from libcpp.vector cimport vector
 
 cdef extern from "passInf.h":
@@ -15,24 +14,21 @@ cdef extern from "passInf.h":
 cdef extern from "isbfcpp.h":
     cdef cppclass isbfcpp:
         isbfcpp() except +
-        int length()
-        void nth_from_last(int, int &x, int &y)
-        bool addList(int, int)
-        void roateMatrix(int, int, int **input, int **output)
-        vector[int] getList(int, int, int *size, int *mask, int, int, float)
-        void clean()
+        vector[vector[int]] rotateMatrix(int, int, vector[vector[int]] input)
+        vector[vector[int]] traceBoundary(int, int, vector[vector[int]] mask, int, int, float)
 
 def isbf(np.ndarray[int, ndim=2, mode="c"] mask not None, xstart, ystart, INFINITY):
     cdef int m, n
-    cdef int size
     m, n = mask.shape[0], mask.shape[1]
     inf = INFINITY if INFINITY != 0 else float('inf')
     cdef isbfcpp res
     res = isbfcpp()
-    cdef vector[int] boundary = res.getList(m, n, &size, &mask[0,0], xstart, ystart, inf)
-    cdef np.ndarray h = np.zeros([2, size-1], dtype=np.int)
+    cdef vector[vector[int]] boundary = res.traceBoundary(m, n, mask, xstart, ystart, inf)
+    cdef int size
+    size = boundary[0].size()
+    cdef np.ndarray h = np.zeros([2, size], dtype=np.int)
     cdef int i
-    for i in range(size-1):
-      h[0,i] = boundary[i]
-      h[1,i] = boundary[i+size-1]
+    for i in range(size):
+      h[0,i] = boundary[0][i]
+      h[1,i] = boundary[1][i]
     return h[0], h[1]
