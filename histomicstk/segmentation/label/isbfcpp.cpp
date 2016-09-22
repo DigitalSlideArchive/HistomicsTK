087fbe9cf214a6f5e96270b5eb916daa8124100a
Sanghoon Lee <shlee8020@gmail.com>
2016-09-22 10:55:04 -0400

    Updated isbfcpp.cpp with std

--- histomicstk/segmentation/label/isbfcpp.cpp
+++ histomicstk/segmentation/label/isbfcpp.cpp
@@ -1,154 +1,81 @@
 /*
 C++ version of ISBF for TraceBounds
 */
-#include <iostream>
-#include <math.h>
 #include "isbfcpp.h"
 
-using namespace std;
+#include <iostream>
+#include <list>
+#include <cmath>
+#include <vector>
 
-isbfcpp::isbfcpp()
+isbfcpp::isbfcpp(){}
+
+std::vector <std::vector<int> > isbfcpp::rotateMatrix(int nrows, int ncols, std::vector <std::vector<int> > input)
 {
-    head = NULL;
-    curr = NULL;
-}
-
-int isbfcpp::length()
-{
-  node *cur = head;
-  int len = 0;
-	while(cur) {
-		cur = cur->next;
-    len++;
-	}
-  return len;
-}
-
-void isbfcpp::nth_from_last(int n, int &x, int &y)
-{
-    node *cur = head;
-    int count = 0;
-  	while(cur) {
-        if (count == n) {
-            x = cur->val_x;
-            y = cur->val_y;
-        }
-    		cur = cur->next;
-        count++;
-  	}
-}
-
-bool isbfcpp::addList(int x, int y)
-{
-  node *newNode = new node;
-  newNode->val_x = x;
-  newNode->val_y = y;
-  newNode->next = NULL;
-
-  if (head == NULL) {
-    head = new node;
-    curr = new node;
-    head = curr = newNode;
-  }
-  else {
-    curr-> next = newNode;
-    curr = newNode;
-  }
-
-  count++;
-  return true;
-}
-
-void isbfcpp::rotateMatrix(int rows, int cols, int **input, int **output)
-{
-    int i, j;
-    for (i=0; i<rows; i++){
-      for (j=0;j<cols; j++){
-        output[j][rows-1-i] = input[i][j];
+    std::vector <std::vector<int> > output(ncols, std::vector<int>(nrows));
+    for (int i=0; i<nrows; i++){
+      for (int j=0;j<ncols; j++){
+        output[j][nrows-1-i] = input[i][j];
       }
     }
+    return output;
 }
 
-void isbfcpp::clean()
+std::vector <std::vector<int> > isbfcpp::traceBoundary(int nrows, int ncols, std::vector <std::vector<int> > mask, int startX, int startY, float inf)
 {
-    for (int i = 0; i < nrows; i++) {
-        delete[] matrix00[i];
-        delete[] matrix180[i];
-    }
-    for (int i = 0; i < ncols; i++) {
-        delete[] matrix90[i];
-        delete[] matrix270[i];
-    }
-    delete[] matrix00;
-    delete[] matrix90;
-    delete[] matrix180;
-    delete[] matrix270;
-}
+    // initialize boundary vector
+    std::list<int> boundary_listX;
+    std::list<int> boundary_listY;
 
-vector<int> isbfcpp::getList(int rows, int cols, int *size, int *mask, int startX, int startY, float inf)
-{
-    addList(startX, startY);
+    // push the first x and y points
+    boundary_listX.push_back(startX);
+    boundary_listY.push_back(startY);
 
-    nrows = rows;
-    ncols = cols;
-
-    matrix00 = new int*[nrows];
-    for (int i = 0; i < nrows; i++) {
-      matrix00[i] = new int[ncols];
-    }
+    // initialize matrix for 0, 90, 180, 270 degrees
+    std::vector <std::vector<int> > matrix00(nrows, std::vector<int>(ncols));
+    std::vector <std::vector<int> > matrix90(ncols, std::vector<int>(nrows));
+    std::vector <std::vector<int> > matrix180(nrows, std::vector<int>(ncols));
+    std::vector <std::vector<int> > matrix270(ncols, std::vector<int>(nrows));
 
     // copy mask to matrix00
     for(int i=0; i< nrows; i++){
         for(int j=0; j< ncols; j++){
-            matrix00[i][j] = mask[i*ncols+j];
+            //matrix00[i][j] = mask[i*ncols+j];
+            matrix00[i][j] = mask[i][j];
         }
     }
 
-    matrix90 = new int*[ncols];
-    for (int i = 0; i < ncols; i++) {
-      matrix90[i] = new int[nrows];
-    }
-    matrix180 = new int*[nrows];
-    for (int i = 0; i < nrows; i++) {
-      matrix180[i] = new int[ncols];
-    }
-    matrix270 = new int*[ncols];
-    for (int i = 0; i < ncols; i++) {
-      matrix270[i] = new int[nrows];
-    }
-
-    // rotate Matrix for 90, 180, 270 degrees
-    rotateMatrix(nrows, ncols, matrix00, matrix270);
-    rotateMatrix(ncols, nrows, matrix270, matrix180);
-    rotateMatrix(nrows, ncols, matrix180, matrix90);
+    // rotate matrix for 90, 180, 270 degrees
+    matrix270 = rotateMatrix(nrows, ncols, matrix00);
+    matrix180 = rotateMatrix(ncols, nrows, matrix270);
+    matrix90 = rotateMatrix(nrows, ncols, matrix180);
 
     // set defalut direction
     int DX = 1;
     int DY = 0;
+
     // set the number of rows and cols for ISBF
     int rowISBF = 3;
     int colISBF = 2;
 
     float angle;
+
     // set size of X: the size of X is equal to the size of Y
     int sizeofX;
+
     // loop until true
     while(1) {
 
-      int x;
-      int y;
-
-      x = curr->val_x;
-      y = curr->val_y;
-
-      int **h = new int*[rowISBF];
-      for (int i = 0; i < rowISBF; i++) {
-        h[i] = new int[colISBF];
-      }
+      std::vector <std::vector<int> > h(rowISBF, std::vector<int>(colISBF));
 
       // initialize a and b which are indices of ISBF
       int a = 0;
       int b = 0;
+
+      int x = boundary_listX.back();
+      int y = boundary_listY.back();
+
+      //cout << DX << " " << DY << endl;
 
       if((DX == 1)&&(DY == 0)){
         for (int i = ncols-x-2; i < ncols-x+1; i++) {
@@ -198,12 +125,9 @@ vector<int> isbfcpp::getList(int rows, int cols, int *size, int *mask, int start
         angle = M_PI;
       }
 
-      // initialize cX and cY
-      // cX and cY indicate directions for each ISBF
-      vector<int> cX(1);
-      vector<int> cY(1);
-      cX.at(0)=0;
-      cY.at(0)=0;
+      // initialize cX and cY which indicate directions for each ISBF
+      std::vector<int> cX(1);
+      std::vector<int> cY(1);
 
       if (h[1][0] == 1) {
           // 'left' neighbor
@@ -226,8 +150,8 @@ vector<int> isbfcpp::getList(int rows, int cols, int *size, int *mask, int start
                       // inner corner at front
                       cX.at(0) = 0;
                       cY.at(0) = -1;
-                      cX.resize(2,(int)-1);
-                      cY.resize(2,(int)0);
+                      cX.push_back(-1);
+                      cY.push_back(0);
                       DX = 0;
                       DY = -1;
                   }
@@ -236,7 +160,7 @@ vector<int> isbfcpp::getList(int rows, int cols, int *size, int *mask, int start
                       cX.at(0) = -1;
                       cY.at(0) = -1;
                       DX = 0;
-                      DY = 1;
+                      DY = -1;
                   }
               }
               else if(h[0][1] == 1){
@@ -254,91 +178,70 @@ vector<int> isbfcpp::getList(int rows, int cols, int *size, int *mask, int start
           }
       }
 
-      // free ISBF matrix
-      for (int i = 0; i < rowISBF; i++) {
-          delete[] h[i];
-      }
-      delete[] h;
-
       // transform points by incoming directions and add to contours
       float s = sin(angle);
       float c = cos(angle);
 
       if(!((cX.at(0)==0)&&(cY.at(0)==0))){
-          for(int t=0; t<int(cX.size()); t++){
-
+          for(int t=0; t< int(cX.size()); t++){
               float a, b;
               int cx = cX.at(t);
               int cy = cY.at(t);
 
               a = c*cx - s*cy;
               b = s*cx + c*cy;
-              x = curr->val_x;
-              y = curr->val_y;
 
-              addList(x+roundf(a), y+roundf(b));
+              x = boundary_listX.back();
+              y = boundary_listY.back();
+
+              boundary_listX.push_back(x+roundf(a));
+              boundary_listY.push_back(y+roundf(b));
           }
       }
 
       float i, j;
-      i = c*DX-s*DY;
-      j = s*DX+c*DY;
+      i = c*DX - s*DY;
+      j = s*DX + c*DY;
       DX = roundf(i);
       DY = roundf(j);
 
       // get length of the current linked list
-      sizeofX = length();
+      sizeofX = boundary_listX.size();
 
       if (sizeofX > 3) {
-          int fx1 = head->val_x;
-          int fx2 = head->next->val_x;
-          int fy1 = head->val_y;
-          int fy2 = head->next->val_y;
-          int lx1 = curr->val_x;
-          int ly1 = curr->val_y;
-          int lx2, ly2;
-          nth_from_last(sizeofX-2, lx2, ly2);
+
           // check if the first and the last x and y are equal
           if ((sizeofX > inf)|| \
-            ((lx1 == fx2)&&(lx2 == fx1)&&(ly1 == fy2)&&(ly2 == fy1))){
-                // delete the last node
-                node *c = head;
-                node *temp =  head;
-                while(c->next != NULL){
-                  temp = c;
-                  c = c->next;
-                }
-                temp->next = NULL;
-
-                break;
+            ((*std::prev(boundary_listX.end()) == *std::next(boundary_listX.begin(), 1))&&
+            (*std::prev(boundary_listX.end(), 2) == *boundary_listX.begin())&&
+            (*std::prev(boundary_listY.end()) == *std::next(boundary_listY.begin(), 1))&&
+            (*std::prev(boundary_listY.end(), 2) == *boundary_listY.begin()))){
+              break;
           }
       }
     }
+
     // allocate memory for return value
-    vector<int> array(2*sizeofX-2);
-    int index = 0;
-    while (head != NULL) {
-        array[index] = head->val_x;
-        array[index+sizeofX-1] = head->val_y;
-        head = head->next;
-        index++;
-    }
+    std::vector <std::vector<int> > boundaries(2, std::vector<int>(sizeofX));
 
-    // get size of x
-    *size = sizeofX;
+    // remove the last element
+    boundary_listX.pop_back();
+    boundary_listY.pop_back();
+    /*
+    std::vector<int> bX { std::make_move_iterator(begin(boundary_listX)),
+                     std::make_move_iterator(end(boundary_listX)) };
 
-    clean();
+    std::vector<int> bY { std::make_move_iterator(begin(boundary_listY)),
+                    std::make_move_iterator(end(boundary_listY)) };
+    */
+    std::vector<int> bX (boundary_listX.begin(), boundary_listX.end());
 
-    return array;
+    std::vector<int> bY (boundary_listY.begin(), boundary_listY.end());
+
+    boundaries[0] = bX;
+    boundaries[1] = bY;
+
+    return boundaries;
 }
 
-isbfcpp::~isbfcpp()
-{
-    node *c = head;
-    while (c != NULL) {
-        head = head->next;
-        delete c;
-        c = head;
-    }
-    curr = head = NULL;
-}
+isbfcpp::~isbfcpp(){}
