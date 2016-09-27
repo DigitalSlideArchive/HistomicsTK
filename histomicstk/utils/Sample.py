54950fe1b57494cedc9e67269ec42e1ae3629817
deepak.chittajallu <cdeepakroy@gmail.com>
2016-09-27 00:45:18 -0400

    Used large_image tileIterator to implement Sample.py

--- histomicstk/utils/Sample.py
+++ histomicstk/utils/Sample.py
@@ -1,10 +1,7 @@
 import large_image
 import numpy as np
-import openslide
 import scipy
 
-from .TilingSchedule import TilingSchedule
-from .ConvertSchedule import ConvertSchedule
 from .SimpleMask import SimpleMask
 
 
@@ -44,90 +41,45 @@ def Sample(slide_path, magnification, percent, tile_size,
     histomicstk.preprocessing.color_deconvolution.SparseColorDeconvolution
     """
 
-    # open image
-    Slide = openslide.OpenSlide(slide_path)
+    ts = large_image.getTileSource(slide_path)
 
-    # generate tiling schedule for desired sampling magnification
-    Schedule = TilingSchedule(slide_path, magnification, tile_size)
+    # get enitre whole-silde image at low resolution
+    im_lowres = ts.getRegion(scale={'magnification': mapping_mag},
+                             format=large_image.tilesource.TILE_FORMAT_NUMPY)
 
-    # convert tiling schedule to low-resolution for tissue mapping
-    lrSchedule = ConvertSchedule(Schedule, mapping_mag)
+    # compute foreground mask of whole-slide image at low-res
+    fgnd_mask_low_res = SimpleMask(im_lowres)
 
-    # get width, height of image at low-res reading magnification
-    lrHeight = Slide.level_dimensions[lrSchedule.Level][1]
-    lrWidth = Slide.level_dimensions[lrSchedule.Level][0]
+    # generate sample pixels
+    sample_pixels = []
 
-    # NEED TO CHECK lrSchedule.Factor to make sure we don't read a
-    # huge buffer in. This would only happen if there are no
-    # low-resolution images available
+    for tile in ts.tileIterator(
+            format=large_image.tilesource.TILE_FORMAT_NUMPY,
+            scale={'magnification': magnification}):
 
-    # read in whole slide at low magnification
-    LR = Slide.read_region((0, 0), lrSchedule.Level, (lrWidth, lrHeight))
+        # get current tile image
+        im_tile = tile['tile'][:, :, :3]
 
-    # convert to numpy array and strip alpha channel
-    LR = np.asarray(LR)
-    LR = LR[:, :, :3]
+        # upsample fgnd mask to desired sampling resolution
+        fgnd_mask = scipy.misc.imresize(fgnd_mask_low_res,
+                                        im_tile.shape,
+                                        interp='nearest')
 
-    # resize if desired magnification is not provided by the file
-    if lrSchedule.Factor != 1.0:
-        LR = scipy.misc.imresize(LR, lrSchedule.Factor)
-        lrHeight = LR.shape[0]
-        lrWidth = LR.shape[1]
+        # generate linear indices of sample pixels in fgnd mask
+        nz_ind = np.nonzero(fgnd_mask.flatten())[0]
+        sample_ind = np.random.choice(nz_ind, np.ceil(percent * nz_ind.size))
 
-    # mask
-    Mask = SimpleMask(LR)
+        # convert rgb tile image to 3xN array
+        tile_pix_rgb = np.reshape(im_tile,
+                                  (im_tile.shape[0] * im_tile.shape[1], 3))
 
-    # pad mask to match overall size of evenly tiled image
-    MaskHeight = lrSchedule.Tout * lrSchedule.Y.shape[0]
-    MaskWidth = lrSchedule.Tout * lrSchedule.X.shape[1]
-    LRMask = np.zeros((MaskHeight, MaskWidth), dtype=np.uint8)
-    LRMask[0:lrHeight, 0:lrWidth] = Mask
-
-    # sample from tile at full resolution that contain more than 1/2 foreground
-    Pixels = list()
-    for i in range(Schedule.X.shape[0]):
-        for j in range(Schedule.X.shape[1]):
-            lrTileMask = LRMask[
-                i * lrSchedule.Tout:(i + 1) * lrSchedule.Tout,
-                j * lrSchedule.Tout:(j + 1) * lrSchedule.Tout].astype(np.uint8)
-            TissueCount = sum(lrTileMask.flatten().astype(
-                np.float)) / (lrSchedule.Tout**2)
-            if TissueCount > min_coverage:
-                # region from desired magnfication
-                tile_size = Slide.read_region((int(Schedule.X[i, j]),
-                                               int(Schedule.Y[i, j])),
-                                              Schedule.Level,
-                                              (Schedule.Tout, Schedule.Tout))
-                tile_size = np.asarray(tile_size)
-                tile_size = tile_size[:, :, :3]
-
-                # resize if desired magnification is not provided by the file
-                if Schedule.Factor != 1.0:
-                    tile_size = scipy.misc.imresize(tile_size,
-                                                    Schedule.Factor,
-                                                    interp='nearest')
-
-                # upsample tile mask from low-resolution to high-resolution
-                TileMask = scipy.misc.imresize(lrTileMask,
-                                               tile_size.shape,
-                                               interp='nearest')
-
-                # generate linear indices of pixels in mask
-                Indices = np.nonzero(TileMask.flatten())[0]
-                Sampling = np.random.uniform(0, Indices.size,
-                                             (np.ceil(percent *
-                                                      Indices.size),))
-                Indices = Indices[Sampling.astype(np.int)]
-
-                # convert rgb tile to 3xN array and sample with linear indices
-                Vectorized = np.reshape(tile_size,
-                                        (tile_size.shape[0] * tile_size.shape[1], 3))
-                Pixels.append(Vectorized[Indices, :].transpose())
+        # add rgb triplet of sample pixels
+        sample_pixels.append(tile_pix_rgb[sample_ind, :])
 
     # concatenate pixel values in list
     try:
-        Pixels = np.concatenate(Pixels, 1)
+        sample_pixels = np.concatenate(Pixels, 1)
     except ValueError:
         print "Sampling could not identify any foreground regions."
 
-    return Pixels
+    return sample_pixels
