45f5cf8ca0539f3bae83c31376a8a4db9ff9c798
Sanghoon Lee <shlee8020@gmail.com>
2016-10-01 22:58:41 -0400

    Modified plugin.cmake

--- plugin.cmake
+++ plugin.cmake
@@ -54,8 +54,8 @@ add_python_test(glcm
 )
 
 add_python_test(segmentation_label
-    SUBMODULE TraceBoundsTest
-    DBNAME core_segmentation_trace_bounds
+    SUBMODULE TraceBoundaryTest
+    DBNAME core_segmentation_trace_boundary
     PLUGIN HistomicsTK
 )
 
