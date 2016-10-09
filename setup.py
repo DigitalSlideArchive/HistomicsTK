f839e0d4b8ea647d17288421f634ef92433efebb
Sanghoon Lee <shlee8020@gmail.com>
2016-10-09 12:07:58 -0400

    Added include_path to setup.py

--- setup.py
+++ setup.py
@@ -78,5 +78,7 @@ setup(name='histomicstk',
       ],
       test_suite='plugin_tests',
       tests_require=test_requirements,
-      ext_modules = cythonize('histomicstk/segmentation/label/*.pyx'),
+      ext_modules = cythonize('histomicstk/segmentation/label/*.pyx',
+                              include_path = [numpy.get_include()]
+                             ),
       )
