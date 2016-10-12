a59736e3df893b8f1a804df504778f23f83724a6
Sanghoon Lee <shlee8020@gmail.com>
2016-10-12 14:47:15 -0400

    Update trace_boundary_cpp.h to handle multiple objects

--- histomicstk/segmentation/label/trace_boundary_cpp.h
+++ histomicstk/segmentation/label/trace_boundary_cpp.h
@@ -16,12 +16,16 @@ class trace_boundary_cpp
 
 public:
     trace_boundary_cpp();
-    void rot90(int nrows, int ncols, std::vector <std::vector<int> > matrix,
-               std::vector <std::vector<int> > &matrix270,
-               std::vector <std::vector<int> > &matrix180,
-               std::vector <std::vector<int> > &matrix90);
-    std::vector <std::vector<int> > isbf(int nrows, int ncols, std::vector <std::vector<int> > mask, int startX, int startY, float inf);
-    std::vector <std::vector<int> > moore(int nrows, int ncols, std::vector <std::vector<int> > mask, int startX, int startY, float inf);
+    void rot90(int nrows, int ncols,
+               std::vector <std::vector<int> > input,
+               std::vector <std::vector<int> > &output);
+    std::vector <std::vector<std::vector<int>> > trace_boundary(std::vector <std::vector<int> > imLables, int connectivity);
+    std::vector <std::vector<int> > isbf(int nrows, int ncols, std::vector <std::vector<int> > mask,
+      std::vector <std::vector<int> > mask_90, std::vector <std::vector<int> > mask_180,
+      std::vector <std::vector<int> > mask_270, int startX, int startY, float inf);
+    std::vector <std::vector<int> > moore(int nrows, int ncols, std::vector <std::vector<int> > mask,
+      std::vector <std::vector<int> > mask_90, std::vector <std::vector<int> > mask_180,
+      std::vector <std::vector<int> > mask_270, int startX, int startY, float inf);
     ~trace_boundary_cpp();
 
 };
