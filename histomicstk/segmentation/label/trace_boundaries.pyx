40a107d6b7674c8f0eb14e6d07d7254ca37343e8
slee172 <shlee8020@gmail.com>
2016-10-18 02:51:08 -0400

    Set defalut parameters in  trace_boundaries.pyx

--- histomicstk/segmentation/label/trace_boundaries.pyx
+++ histomicstk/segmentation/label/trace_boundaries.pyx
@@ -1,21 +1,24 @@
 """
-Cython source code: trace_boundaries
+Cython source code: trace_boundaries.pyx
 """
 import cython
 
 import numpy as np
 cimport numpy as np
-
+cimport trace_boundaries_opt
 from libcpp.vector cimport vector
 
-cdef extern from "trace_boundaries_opt.h":
-  vector[vector[vector[int]]] trace_boundary(vector[vector[int]] label, int)
 
-
-def trace_boundaries(np.ndarray[int, ndim=2, mode="c"] imLabel not None, connectivity=4):
+def trace_boundaries(np.ndarray[np.int64_t, ndim=2, mode="c"] imLabel not None, Connectivity=4, XStart=None, YStart=None, MaxLength=None):
 
     cdef vector[vector[vector[int]]] output
+    inf = MaxLength if MaxLength != None else float('inf')
 
-    output = trace_boundary(imLabel, connectivity)
+    if XStart is None and YStart is None:
+        output = trace_boundaries_opt.trace_boundary(imLabel, Connectivity, inf)
+    elif XStart is not None and YStart is not None:
+        output = trace_boundaries_opt.trace_boundary(imLabel, Connectivity, inf, XStart, YStart)
+    else:
+        raise ValueError("XStart or YStart is not defined !!")
 
     return output
