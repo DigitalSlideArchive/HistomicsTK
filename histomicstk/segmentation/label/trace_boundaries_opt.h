4602f3f8b0a58c297241c89e3cb6030536bd6d45
slee172 <shlee8020@gmail.com>
2016-10-26 00:22:43 -0400

    Added trace_label to trace_boundaries_opt.h

--- histomicstk/segmentation/label/trace_boundaries_opt.h
+++ histomicstk/segmentation/label/trace_boundaries_opt.h
@@ -13,6 +13,8 @@ Header of trace_boundaries_opt
 void rot90(int nrows, int ncols, std::vector <std::vector<int> > input, std::vector <std::vector<int> > &output);
 std::vector <std::vector<std::vector<int>> > trace_boundary(std::vector <std::vector<int> > imLables, int connectivity=4,
     float inf=INFINITY, int startX=-1, int startY=-1);
+std::vector <std::vector<std::vector<int>> > trace_label(std::vector <std::vector<int> > imLables, int connectivity=4,
+    float inf=INFINITY);
 std::vector <std::vector<int> > isbf(int nrows, int ncols, std::vector <std::vector<int> > mask,
     std::vector <std::vector<int> > mask_90, std::vector <std::vector<int> > mask_180,
     std::vector <std::vector<int> > mask_270, int startX, int startY, float inf);
