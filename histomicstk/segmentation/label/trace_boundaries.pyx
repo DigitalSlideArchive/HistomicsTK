3616795e4d008d53a7a8a335b4424498e196fed6
slee172 <shlee8020@gmail.com>
2016-10-26 00:14:09 -0400

    Added trace_label to trace_boundaries.pyx

--- histomicstk/segmentation/label/trace_boundaries.pyx
+++ histomicstk/segmentation/label/trace_boundaries.pyx
@@ -9,16 +9,18 @@ cimport trace_boundaries_opt
 from libcpp.vector cimport vector
 
 
-def trace_boundaries(np.ndarray[np.int64_t, ndim=2, mode="c"] imLabel not None, Connectivity=4, XStart=None, YStart=None, MaxLength=None):
+def trace_boundaries(np.ndarray[np.int64_t, ndim=2, mode="c"] imLabel not None, Connectivity=4, Label=False, XStart=None, YStart=None, MaxLength=None):
 
     cdef vector[vector[vector[int]]] output
     inf = MaxLength if MaxLength != None else float('inf')
-
-    if XStart is None and YStart is None:
-        output = trace_boundaries_opt.trace_boundary(imLabel, Connectivity, inf)
-    elif XStart is not None and YStart is not None:
-        output = trace_boundaries_opt.trace_boundary(imLabel, Connectivity, inf, XStart, YStart)
+    if Label:
+        output = trace_boundaries_opt.trace_label(imLabel, Connectivity, inf)
     else:
-        raise ValueError("XStart or YStart is not defined !!")
+        if XStart is None and YStart is None:
+            output = trace_boundaries_opt.trace_boundary(imLabel, Connectivity, inf)
+        elif XStart is not None and YStart is not None:
+            output = trace_boundaries_opt.trace_boundary(imLabel, Connectivity, inf, XStart, YStart)
+        else:
+            raise ValueError("XStart or YStart is not defined !!")
 
     return output
