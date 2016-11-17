f388f8121e096743c933f5c6c3a5fe51bb119edd
Deepak Roy Chittajallu <deepak.chittajallu@kitware.com>
2016-11-17 17:49:35 -0500

    Rename function arguments of gaussian_grad

--- histomicstk/filters/edge/gaussian_grad.py
+++ histomicstk/filters/edge/gaussian_grad.py
@@ -3,7 +3,7 @@ import numpy as np
 import scipy.signal as signal
 
 
-def gaussian_grad(I, Sigma):
+def gaussian_grad(im_input, sigma):
     """Performs smoothing with derivative gaussian kernel.
 
     Uses seperable convolution to simultaneously smooth and calculate the
@@ -11,16 +11,16 @@ def gaussian_grad(I, Sigma):
 
     Parameters
     ----------
-    I : array_like
+    im_input : array_like
         An intensity image.
-    sSigma : double
+    sigma : double
         Standard deviation of smoothing kernel used in gradient calculation.
 
     Returns
     -------
-    dX : array_like
+    dx : array_like
         An intensity image of the X gradient component.
-    dY : array_like
+    dy : array_like
         An intensity image of the Y gradient component.
 
     Notes
@@ -30,27 +30,27 @@ def gaussian_grad(I, Sigma):
     """
 
     # generate separable gaussian derivative kernels
-    x = np.linspace(0, 2 * 3 * Sigma, 2 * 3 * Sigma + 1)
-    y = np.linspace(0, 2 * 3 * Sigma, 2 * 3 * Sigma + 1)
-    x -= 2 * 3 * Sigma / 2  # center independent variables at zero
-    y -= 2 * 3 * Sigma / 2
+    x = np.linspace(0, 2 * 3 * sigma, 2 * 3 * sigma + 1)
+    y = np.linspace(0, 2 * 3 * sigma, 2 * 3 * sigma + 1)
+    x -= 2 * 3 * sigma / 2  # center independent variables at zero
+    y -= 2 * 3 * sigma / 2
     x = np.reshape(x, (1, x.size))  # reshape to 2D row and column vectors
     y = np.reshape(y, (y.size, 1))
-    xGx = 2 * x / (Sigma**2) * np.exp(-x**2 / (2 * Sigma**2)) \
-        / (Sigma * (2 * np.pi) ** 0.5)
-    yGx = np.exp(-y**2 / (2 * Sigma**2)) / ((2 * np.pi) ** 0.5 * Sigma)
-    xGy = np.exp(-x**2 / (2 * Sigma**2)) / ((2 * np.pi) ** 0.5 * Sigma)
-    yGy = 2 * y / (Sigma**2) * np.exp(-y**2 / (2 * Sigma**2)) \
-        / (Sigma * (2 * np.pi) ** 0.5)
+    xGx = 2 * x / (sigma ** 2) * np.exp(-x ** 2 / (2 * sigma ** 2)) \
+        / (sigma * (2 * np.pi) ** 0.5)
+    yGx = np.exp(-y**2 / (2 * sigma ** 2)) / ((2 * np.pi) ** 0.5 * sigma)
+    xGy = np.exp(-x**2 / (2 * sigma ** 2)) / ((2 * np.pi) ** 0.5 * sigma)
+    yGy = 2 * y / (sigma ** 2) * np.exp(-y ** 2 / (2 * sigma ** 2)) \
+        / (sigma * (2 * np.pi) ** 0.5)
 
     # smoothed gradients of input image
-    dX = signal.convolve2d(I, xGx, mode='same')
-    dX = signal.convolve2d(dX, yGx, mode='same')
-    dY = signal.convolve2d(I, xGy, mode='same')
-    dY = signal.convolve2d(dY, yGy, mode='same')
+    dx = signal.convolve2d(im_input, xGx, mode='same')
+    dx = signal.convolve2d(dx, yGx, mode='same')
+    dy = signal.convolve2d(im_input, xGy, mode='same')
+    dy = signal.convolve2d(dy, yGy, mode='same')
 
     # format output
-    Output = collections.namedtuple('Output', ['dX', 'dY'])
-    Gradients = Output(dX, dY)
+    Output = collections.namedtuple('Output', ['dx', 'dy'])
+    Gradients = Output(dx, dy)
 
     return Gradients
