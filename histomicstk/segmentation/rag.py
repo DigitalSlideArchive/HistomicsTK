abb9b912a6478a3beec3d3639e758917d2e8cc5a
Deepak Roy Chittajallu <deepak.chittajallu@kitware.com>
2016-11-18 11:37:32 -0500

    PEP8 renaming of function args in rag.py

--- histomicstk/segmentation/rag.py
+++ histomicstk/segmentation/rag.py
@@ -1,79 +1,83 @@
 import numpy as np
 
 
-def rag(Label, Neighbors=4):
+def rag(im_label, neigh_conn=4):
     """Constructs a region adjacency graph for a label image using either
     4-neighbor or 8-neighbor connectivity. Background pixels are not included
-    (Label == 0). Not intended to build large graphs from individual pixels.
+    (im_label == 0). Not intended to build large graphs from individual pixels.
 
     Parameters
     ----------
-    Label : array_like
-        Label image where positive values (Label > 0) correspond to foreground
+    im_label : array_like
+        im_label image where positive values (im_label > 0) correspond to foreground
         objects of interest.
 
-    Neighbors : float
+    neigh_conn : float
         The neighbor connectivity to use, either '4' or '8'. Default value = 4.
 
     Returns
     -------
-    Adjacency : array_like
+    adj_mat : array_like
         A binary matrix of size N x N, where N is the number of objects in
-        Label. A value of 'True' at Adjacency(i,j) indicates that objects 'i'
-        and 'j' are neighbors.
+        im_label. A value of 'True' at adj_mat(i,j) indicates that objects 'i'
+        and 'j' are neigh_conn.
     """
 
-    # initialize adjacency matrix for Label.max() total regions
-    Adjacency = np.zeros((Label.max(), Label.max()), dtype=np.bool)
+    # initialize adjacency matrix for im_label.max() total regions
+    adj_mat = np.zeros((im_label.max(), im_label.max()), dtype=np.bool)
 
-    # process 4-neighbors horizontal connections
-    Mask = (Label[:, 1:-1] != Label[:, 0:-2]) & \
-           (Label[:, 1:-1] != 0) & (Label[:, 0:-2] != 0)
+    # process 4-neigh_conn horizontal connections
+    Mask = (im_label[:, 1:-1] != im_label[:, 0:-2]) & \
+           (im_label[:, 1:-1] != 0) & (im_label[:, 0:-2] != 0)
     Indices = np.nonzero(Mask)
-    Xc = np.concatenate((Label[Indices[0], Indices[1]][:, np.newaxis],
-                         Label[Indices[0], Indices[1]+1][:, np.newaxis]),
+    Xc = np.concatenate((im_label[Indices[0], Indices[1]][:, np.newaxis],
+                         im_label[Indices[0], Indices[1] + 1][:, np.newaxis]),
                         axis=1)
 
-    # process 4-neighbors vertical connections
-    Mask = (Label[1:-1, :] != Label[0:-2, :]) & \
-           (Label[1:-1, :] != 0) & (Label[0:-2, :] != 0)
+    # process 4-neigh_conn vertical connections
+    Mask = (im_label[1:-1, :] != im_label[0:-2, :]) & \
+           (im_label[1:-1, :] != 0) & (im_label[0:-2, :] != 0)
     Indices = np.nonzero(Mask)
     Xc = np.concatenate((Xc,
-                         np.concatenate((Label[Indices[0], Indices[1]]
+                         np.concatenate((im_label[Indices[0], Indices[1]]
                                          [:, np.newaxis],
-                                         Label[Indices[0]+1, Indices[1]]
+                                         im_label[Indices[0] + 1, Indices[1]]
                                          [:, np.newaxis]), axis=1)),
                         axis=0)
 
     # process additional 8-neighbor relationships
-    if Neighbors == 8:
+    if neigh_conn == 8:
 
         # shift upper-right ([1, 1])
-        Mask = (Label[1:-1, 0:-2] != Label[0:-2, 1:-1]) & \
-               (Label[1:-1, 0:-2] != 0) & (Label[0:-2, 1:-1] != 0)
+        Mask = (im_label[1:-1, 0:-2] != im_label[0:-2, 1:-1]) & \
+               (im_label[1:-1, 0:-2] != 0) & (im_label[0:-2, 1:-1] != 0)
         Indices = np.nonzero(Mask)
-        Xc = np.concatenate((Xc,
-                             np.concatenate((Label[Indices[0]+1, Indices[1]]
-                                             [:, np.newaxis],
-                                             Label[Indices[0], Indices[1]+1]
-                                             [:, np.newaxis]), axis=1)),
-                            axis=0)
+        Xc = np.concatenate((
+            Xc,
+            np.concatenate((
+                im_label[Indices[0] + 1, Indices[1]][:, np.newaxis],
+                im_label[Indices[0], Indices[1] + 1][:, np.newaxis]),
+                axis=1)),
+            axis=0
+        )
 
         # shift upper-left ([-1, 1])
-        Mask = (Label[0:-2, 0:-2] != Label[1:-1, 1:-1]) & \
-               (Label[0:-2, 0:-2] != 0) & (Label[1:-1, 1:-1] != 0)
+        Mask = (im_label[0:-2, 0:-2] != im_label[1:-1, 1:-1]) & \
+               (im_label[0:-2, 0:-2] != 0) & (im_label[1:-1, 1:-1] != 0)
         Indices = np.nonzero(Mask)
-        Xc = np.concatenate((Xc,
-                             np.concatenate((Label[Indices[0], Indices[1]]
-                                             [:, np.newaxis],
-                                             Label[Indices[0]+1, Indices[1]+1]
-                                             [:, np.newaxis]), axis=1)),
-                            axis=0)
+        Xc = np.concatenate((
+            Xc,
+            np.concatenate((
+                im_label[Indices[0], Indices[1]][:, np.newaxis],
+                im_label[Indices[0] + 1, Indices[1] + 1][:, np.newaxis]),
+                axis=1)),
+            axis=0
+        )
 
     # add entries to adjacency matrix
     for i in range(Xc.shape[0]):
-        Adjacency[Xc[i, 0]-1, Xc[i, 1]-1] = True
-        Adjacency[Xc[i, 1]-1, Xc[i, 0]-1] = True
+        adj_mat[Xc[i, 0]-1, Xc[i, 1]-1] = True
+        adj_mat[Xc[i, 1]-1, Xc[i, 0]-1] = True
 
     # return result
-    return Adjacency
+    return adj_mat
