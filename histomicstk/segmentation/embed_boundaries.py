eef04a71f3caa713880bdaafd1bd78a0c48b830e
Deepak Roy Chittajallu <deepak.chittajallu@kitware.com>
2016-11-18 11:30:06 -0500

    PEP8 renaming of embed_boundaries function args

--- histomicstk/segmentation/embed_boundaries.py
+++ histomicstk/segmentation/embed_boundaries.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 
-def embed_boundaries(I, Bounds, Color=[255, 0, 0]):
+def embed_boundaries(im_input, im_perim, color=[255, 0, 0]):
     """Embeds object boundaries into an RGB color, grayscale or binary
     image, returning a color rendering of the image and object
     boundaries.
@@ -16,57 +16,57 @@ def embed_boundaries(I, Bounds, Color=[255, 0, 0]):
 
     Parameters
     ----------
-    I : array_like
+    im_input : array_like
         A color or grayscale image.
-    Bounds : array_like
+    im_perim : array_like
         A binary image where object perimeter pixels have value 1, and
         non-perimeter pixels have value 0.
-    Color : array_like
+    color : array_like
         A 1 x 3 array of RGB values in the range [0, 255].
 
     Returns
     -------
-    Iout : array_like
+    im_embed : array_like
         A color image of type unsigned char where boundary pixels take
-        on the color defined by the RGB-triplet 'Color'.
+        on the color defined by the RGB-triplet 'color'.
 
     See Also
     --------
     histomicstk.segmentation.label.perimeter
     """
 
-    # check for consistent shapes between 'I' and 'Bounds'
-    if I.shape[0:2] != Bounds.shape[0:2]:
-        raise ValueError("Inputs 'I' and 'Bounds' must have same shape")
+    # check for consistent shapes between 'im_input' and 'im_perim'
+    if im_input.shape[0:2] != im_perim.shape[0:2]:
+        raise ValueError("'im_input' and 'im_perim' must have same shape")
 
     # determine image type
-    if np.issubclass_(I.dtype.type, np.float_):
-        if I.max() < 1.0:
-            I = I * 255
-            I = I.astype(np.uint8)
-    elif np.issubclass_(I.dtype.type, np.bool_):
-        I = I.astype(np.uint8)
-        I = I * 255
+    if np.issubclass_(im_input.dtype.type, np.float_):
+        if im_input.max() < 1.0:
+            im_input = im_input * 255
+            im_input = im_input.astype(np.uint8)
+    elif np.issubclass_(im_input.dtype.type, np.bool_):
+        im_input = im_input.astype(np.uint8)
+        im_input = im_input * 255
 
     # determine if image is grayscale or RGB
-    if len(I.shape) == 3:  # color image
-        Red = I[:, :, 0].copy()
-        Green = I[:, :, 1].copy()
-        Blue = I[:, :, 2].copy()
-    elif len(I.shape) == 2:  # grayscale image
-        Red = I.copy()
-        Green = I.copy()
-        Blue = I.copy()
+    if len(im_input.shape) == 3:  # color image
+        Red = im_input[:, :, 0].copy()
+        Green = im_input[:, :, 1].copy()
+        Blue = im_input[:, :, 2].copy()
+    elif len(im_input.shape) == 2:  # grayscale image
+        Red = im_input.copy()
+        Green = im_input.copy()
+        Blue = im_input.copy()
 
     # embed boundaries
-    Red[Bounds > 0] = Color[0]
-    Green[Bounds > 0] = Color[1]
-    Blue[Bounds > 0] = Color[2]
+    Red[im_perim > 0] = color[0]
+    Green[im_perim > 0] = color[1]
+    Blue[im_perim > 0] = color[2]
 
     # generate output image
-    Iout = np.dstack((Red[:, :, np.newaxis],
+    im_embed = np.dstack((Red[:, :, np.newaxis],
                       Green[:, :, np.newaxis],
                       Blue[:, :, np.newaxis])).astype(np.uint8)
 
     # concatenate channels to form output
-    return Iout
+    return im_embed
