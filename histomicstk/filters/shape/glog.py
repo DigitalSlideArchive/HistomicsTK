ab1da58f685afad9b2cc97e984fd1f15be9bd6fb
Deepak Roy Chittajallu <deepak.chittajallu@kitware.com>
2016-11-18 11:02:14 -0500

    PEP8 Renaming of glog function args

--- histomicstk/filters/shape/glog.py
+++ histomicstk/filters/shape/glog.py
@@ -3,25 +3,27 @@ import scipy.ndimage as ndi
 from skimage import morphology
 
 
-def glog(I, Alpha=1, Range=np.linspace(1.5, 3, np.round((3-1.5)/0.2)+1),
-         Theta=np.pi/4, Tau=0.6, Eps=0.6):
+def glog(im_input, alpha=1,
+         range=np.linspace(1.5, 3, np.round((3 - 1.5) / 0.2) + 1),
+         theta=np.pi/4, tau=0.6, eps=0.6):
     """Performs generalized Laplacian of Gaussian blob detection.
 
     Parameters
     ----------
-    I : array_like
+    im_input : array_like
         A hematoxylin intensity image obtained from ColorDeconvolution.
-    Alpha : double
+    alpha : double
         A positive scalar used to normalize the gLoG filter responses. Controls
         the blob-center detection and eccentricities of detected blobs. Larger
         values emphasize more eccentric blobs. Default value = 1.
-    Range : array_like
-    Theta : double
+    range : array_like
+        Scale range
+    theta : double
         Angular increment for rotating gLoG filters. Default value = np.pi / 6.
-    Tau : double
+    tau : double
         Tolerance for counting pixels in determining optimal scale SigmaC
-    Eps : double
-        Range to define SigmaX surrounding SigmaC
+    eps : double
+        range to define SigmaX surrounding SigmaC
 
     Returns
     -------
@@ -42,7 +44,7 @@ def glog(I, Alpha=1, Range=np.linspace(1.5, 3, np.round((3-1.5)/0.2)+1),
     """
 
     # initialize sigma
-    Sigma = np.exp(Range)
+    Sigma = np.exp(range)
 
     # generate circular LoG scale-space to determine range of SigmaX
     l_g = 0
@@ -51,7 +53,7 @@ def glog(I, Alpha=1, Range=np.linspace(1.5, 3, np.round((3-1.5)/0.2)+1),
     Min = np.zeros((len(Sigma), 1))
     Max = np.zeros((len(Sigma), 1))
     for i, s in enumerate(Sigma):
-        Response = s**2 * ndi.filters.gaussian_laplace(I, s, output=None,
+        Response = s**2 * ndi.filters.gaussian_laplace(im_input, s, output=None,
                                                        mode='constant',
                                                        cval=0.0)
         Min[i] = Response.min()
@@ -67,51 +69,50 @@ def glog(I, Alpha=1, Range=np.linspace(1.5, 3, np.round((3-1.5)/0.2)+1),
     Zeta = np.zeros((len(Sigma), 1))
     for i, s in enumerate(Sigma):
         Bins[i] = (Bins[i] - Min[i]) / (l_g - Min[i])
-        Zeta[i] = np.sum(H[i][Bins[i][0:-1] > Tau])
+        Zeta[i] = np.sum(H[i][Bins[i][0:-1] > tau])
 
     # identify best scale SigmaC based on maximum circular response
     Index = np.argmax(Zeta)
 
     # define range for SigmaX
-    XRange = range(max(Index-2, 0), min(len(Range), Index+2)+1)
-    SigmaX = np.exp(Range[XRange])
+    XRange = range(max(Index-2, 0), min(len(range), Index + 2) + 1)
+    SigmaX = np.exp(range[XRange])
 
     # define rotation angles
-    Thetas = np.linspace(0, np.pi-Theta, np.round(np.pi/Theta))
+    Thetas = np.linspace(0, np.pi - theta, np.round(np.pi / theta))
 
     # loop over SigmaX, SigmaY and then angle, summing up filter responses
-    Rsum = np.zeros(I.shape)
+    Rsum = np.zeros(im_input.shape)
     for i, Sx in enumerate(SigmaX):
         YRange = range(0, XRange[i])
-        SigmaY = np.exp(Range[YRange])
+        SigmaY = np.exp(range[YRange])
         for Sy in SigmaY:
             for Th in Thetas:
                 Kernel = glogkernel(Sx, Sy, Th)
-                Kernel = (1 + np.log(Sx)**Alpha) * (1 + np.log(Sy)**Alpha) *\
-                    Kernel
-                Rsum += ndi.convolve(I, Kernel, mode='constant', cval=0.0)
+                Kernel *= (1 + np.log(Sx) ** alpha) * (1 + np.log(Sy) ** alpha)
+                Rsum += ndi.convolve(im_input, Kernel, mode='constant', cval=0.0)
                 print(Sx, Sy, Th)
         Kernel = glogkernel(Sx, Sx, 0)
-        Kernel = (1 + np.log(Sx)**Alpha) * (1 + np.log(Sx)**Alpha) * Kernel
-        Rsum += ndi.convolve(I, Kernel, mode='constant', cval=0.0)
+        Kernel *= (1 + np.log(Sx) ** alpha) * (1 + np.log(Sx) ** alpha)
+        Rsum += ndi.convolve(im_input, Kernel, mode='constant', cval=0.0)
         print(Sx, Sx, 0)
 
     # detect local maxima
-    Disk = morphology.disk(3*np.exp(Range[Index]))
+    Disk = morphology.disk(3 * np.exp(range[Index]))
     Maxima = ndi.filters.maximum_filter(Rsum, footprint=Disk)
     Maxima = Rsum == Maxima
 
     return Rsum, Maxima
 
 
-def glogkernel(SigmaX, SigmaY, Theta):
-    N = np.ceil(2 * 3 * SigmaX)
+def glogkernel(sigma_x, sigma_y, theta):
+    N = np.ceil(2 * 3 * sigma_x)
     X, Y = np.meshgrid(np.linspace(0, N, N + 1) - N / 2,
                        np.linspace(0, N, N + 1) - N / 2)
-    a = np.cos(Theta)**2 / (2 * SigmaX**2) + np.sin(Theta)**2 / (2 * SigmaY**2)
-    b = -np.sin(2 * Theta) / (4 * SigmaX**2) + np.sin(2 * Theta) /\
-        (4 * SigmaY**2)
-    c = np.sin(Theta)**2 / (2 * SigmaX**2) + np.cos(Theta)**2 / (2 * SigmaY**2)
+    a = np.cos(theta) ** 2 / (2 * sigma_x ** 2) + np.sin(theta) ** 2 / (2 * sigma_y ** 2)
+    b = -np.sin(2 * theta) / (4 * sigma_x ** 2) + np.sin(2 * theta) / \
+                                                  (4 * sigma_y ** 2)
+    c = np.sin(theta) ** 2 / (2 * sigma_x ** 2) + np.cos(theta) ** 2 / (2 * sigma_y ** 2)
     D2Gxx = ((2*a*X + 2*b*Y)**2 - 2*a) * np.exp(-(a*X**2 + 2*b*X*Y + c*Y**2))
     D2Gyy = ((2*b*X + 2*c*Y)**2 - 2*c) * np.exp(-(a*X**2 + 2*b*X*Y + c*Y**2))
     Gaussian = np.exp(-(a*X**2 + 2*b*X*Y + c*Y**2))
