02f75bcddaddae28471d3311e5ef3fb2c34f0145
Deepak Roy Chittajallu <deepak.chittajallu@kitware.com>
2016-11-18 11:22:07 -0500

    PEP8 renaming of chan_vese function args

--- histomicstk/segmentation/level_set/chan_vese.py
+++ histomicstk/segmentation/level_set/chan_vese.py
@@ -3,7 +3,8 @@ from scipy.ndimage.morphology import distance_transform_edt as dtx
 import scipy.ndimage.filters as filters
 
 
-def chan_vese(I, Mask, Sigma, dt=1.0, Mu=0.2, Lambda1=1, Lambda2=1, It=100):
+def chan_vese(im_input, im_mask, sigma,
+              dt=1.0, mu=0.2, lambda1=1, lambda2=1, iter=100):
     """Region-based level sets.
 
     Region-based level set implementation based on the Chan-Vese method.
@@ -13,30 +14,30 @@ def chan_vese(I, Mask, Sigma, dt=1.0, Mu=0.2, Lambda1=1, Lambda2=1, It=100):
 
     Parameters
     ----------
-    I : array_like
+    im_input : array_like
         A floating-point intensity image.
-    Mask : array_like
+    im_mask : array_like
         A binary mask initializing the level set image. Regions corresponding
         to the interior of the level-set function have value 1, with other
         regions having value 0.
-    Sigma : double
-        Standard deviation of smoothing filter for input image I.
+    sigma : double
+        Standard deviation of smoothing filter for input image im_input.
     dt : double
-        Time step for evolving the level-set function Phi. Default value = 1.0.
-    Mu : double
+        Time step for evolving the level-set function im_phi. Default value = 1.0.
+    mu : double
         Boundary length weight for energy function. Default value = 0.2.
-    Lambda1 : double
+    lambda1 : double
         Internal variance weight for energy function. Default value = 1.
-    Lambda2 : double
+    lambda2 : double
         External variance weight for energy function. Default value = 1.
-    It : double
+    iter : double
         Number of iterations to evolve curve level set function over. Default
         value = 100.
 
 
     Returns
     -------
-    Phi : array_like
+    im_phi : array_like
         An intensity image where the zero level set defines object boundaries.
         Can be further processed with fast marching methods or other to obtain
         smooth boundaries, or simply thresholded to define the object mask.
@@ -53,36 +54,38 @@ def chan_vese(I, Mask, Sigma, dt=1.0, Mu=0.2, Lambda1=1, Lambda2=1, It=100):
     """
 
     # smoothed gradient of input image
-    I = filters.gaussian_filter(I, Sigma, mode='constant', cval=0)
+    im_input = filters.gaussian_filter(im_input, sigma, mode='constant', cval=0)
 
     # generate signed distance map
-    Phi = convert_mask(Mask)
+    im_phi = mask_to_sdf(im_mask)
 
     # evolve level set function
-    for i in range(0, It):
+    for i in range(0, iter):
 
         # calculate interior and exterior averages
-        C1 = np.sum(I[Phi > 0]) / (np.sum(Phi > 0) + 1e-10)
-        C2 = np.sum(I[Phi <= 0]) / (np.sum(Phi <= 0) + 1e-10)
-        Force = Lambda2 * (I - C2)**2 - Lambda1 * (I - C1)**2
+        C1 = np.sum(im_input[im_phi > 0]) / (np.sum(im_phi > 0) + 1e-10)
+        C2 = np.sum(im_input[im_phi <= 0]) / (np.sum(im_phi <= 0) + 1e-10)
+        Force = lambda2 * (im_input - C2) ** 2 - lambda1 * (im_input - C1) ** 2
 
         # curvature of image
-        Curvature = kappa(Phi)
+        Curvature = kappa(im_phi)
 
         # evolve
-        Phi += dt * Force / np.max(np.abs(Force)) + Mu*Curvature
+        im_phi += dt * Force / np.max(np.abs(Force)) + mu * Curvature
 
-    return Phi
+    return im_phi
 
 
-def convert_mask(Mask):
+def mask_to_sdf(im_mask):
     # convert binary mask to signed distance function
-    Phi0 = dtx(1-Mask) - dtx(Mask) + Mask - 1/2
+
+    Phi0 = dtx(1 - im_mask) - dtx(im_mask) + im_mask - 1 / 2
     return Phi0
 
 
-def kappa(Phi):
-    dPhi = np.gradient(Phi)  # calculate gradient of level set image
+def kappa(im_phi):
+
+    dPhi = np.gradient(im_phi)  # calculate gradient of level set image
     xdPhi = np.gradient(dPhi[1])
     ydPhi = np.gradient(dPhi[0])
     K = (xdPhi[1]*(dPhi[0]**2) - 2*xdPhi[0]*dPhi[0]*dPhi[1] +
