e317f029518e82ccc2070ffa445b0c490db50e32
deepak.chittajallu <cdeepakroy@gmail.com>
2016-11-21 09:55:07 -0500

    PEP8 renaming of function args of del2

--- histomicstk/utils/del2.py
+++ histomicstk/utils/del2.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 
-def del2(X):
+def del2(im_input):
     """Discrete Laplacian with edge-value extrapolation.
 
     Calculates the discrete Laplacian of an input image. Edge values are
@@ -10,13 +10,13 @@ def del2(X):
 
     Parameters
     ----------
-    X : array_like
+    im_input : array_like
         A floating-point intensity image.
 
     Returns
     -------
-    L : array_like
-        The discrete Laplacian of L.
+    im_lap : array_like
+        The discrete Laplacian of im_lap.
 
     See Also
     --------
@@ -24,36 +24,36 @@ def del2(X):
     """
 
     # initialize output
-    L = np.zeros(X.shape)
+    im_lap = np.zeros(im_input.shape)
 
     # process rows first
-    D = np.diff(X, axis=0)
-    S = np.zeros(X.shape)
+    D = np.diff(im_input, axis=0)
+    S = np.zeros(im_input.shape)
     S[1:-1, :] = (D[1:, :] - D[0:-1, :])/2
-    if X.shape[0] > 3:
+    if im_input.shape[0] > 3:
         S[0, :] = 2 * S[1, :] - S[2, :]
         S[-1, :] = 2 * S[-2, :] - S[-3, :]
-    elif X.shape[0] == 3:
+    elif im_input.shape[0] == 3:
         S[0, :] = S[1, :]
         S[-1, :] = S[1, :]
     else:
         S[0, :] = 0
         S[-1, :] = 0
-    L += S
+    im_lap += S
 
     # process columns
-    D = np.diff(X, axis=1)
-    S = np.zeros(X.shape)
+    D = np.diff(im_input, axis=1)
+    S = np.zeros(im_input.shape)
     S[:, 1:-1] = (D[:, 1:] - D[:, 0:-1])/2
-    if X.shape[1] > 3:
+    if im_input.shape[1] > 3:
         S[:, 0] = 2 * S[:, 1] - S[:, 2]
         S[:, -1] = 2 * S[:, -2] - S[:, -3]
-    elif X.shape[1] == 3:
+    elif im_input.shape[1] == 3:
         S[0, :] = S[:, 1]
         S[:, -1] = S[:, 1]
     else:
         S[:, 0] = 0
         S[:, -1] = 0
-    L += S
+    im_lap += S
 
-    return L / 2
+    return im_lap / 2
