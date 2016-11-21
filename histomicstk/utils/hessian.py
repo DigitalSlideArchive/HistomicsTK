65275a38104aee67995befa0c61ec3f34a2e964f
deepak.chittajallu <cdeepakroy@gmail.com>
2016-11-21 09:55:33 -0500

    PEP8 renaming of function args of hessian

--- histomicstk/utils/hessian.py
+++ histomicstk/utils/hessian.py
@@ -2,45 +2,45 @@ import numpy as np
 from scipy.ndimage.filters import convolve
 
 
-def hessian(I, Sigma):
+def hessian(im_input, sigma):
     """
     Calculates hessian of image I convolved with a gaussian kernel with
     covariance C = [Sigma^2 0; 0 Sigma^2].
 
     Parameters
     ----------
-    I : array_like
+    im_input : array_like
         M x N grayscale image.
-    Sigma : double
+    sigma : double
         standard deviation of gaussian kernel.
 
     Returns
     -------
-    H - array_like
-        M x N x 4 hessian matrix - H[:,:,0] = dxx,
-        H[:,:,1] = H[:,:,2] = dxy, H[:,:,3] = dyy.
+    im_hess - array_like
+        M x N x 4 hessian matrix - im_hess[:,:,0] = dxx,
+        im_hess[:,:,1] = im_hess[:,:,2] = dxy, im_hess[:,:,3] = dyy.
     """
 
     # generate kernel domain
-    h, k = round(3*Sigma), round(3*Sigma+1)
+    h, k = round(3 * sigma), round(3 * sigma + 1)
     x, y = np.mgrid[-h:k, -h:k]
 
     # generate kernels
-    gxx = 1./(2*np.pi*Sigma**4)*((x/Sigma)**2-1) * \
-        np.exp(-(x**2+y**2)/(2*Sigma**2))
-    gxy = 1./(2*np.pi*Sigma**6)*np.multiply(x, y) * \
-        np.exp(-(x**2+y**2)/(2*Sigma**2))
+    gxx = 1./(2 * np.pi * sigma ** 4) * ((x / sigma) ** 2 - 1) * \
+          np.exp(-(x**2+y**2) / (2 * sigma ** 2))
+    gxy = 1./(2 * np.pi * sigma ** 6) * np.multiply(x, y) * \
+          np.exp(-(x**2+y**2) / (2 * sigma ** 2))
     gyy = np.transpose(gxx)
 
     # convolve
-    dxx = convolve(I, gxx, mode='constant')
-    dxy = convolve(I, gxy, mode='constant')
-    dyy = convolve(I, gyy, mode='constant')
+    dxx = convolve(im_input, gxx, mode='constant')
+    dxy = convolve(im_input, gxy, mode='constant')
+    dyy = convolve(im_input, gyy, mode='constant')
 
     # format output
-    H = np.concatenate(
+    im_hess = np.concatenate(
         (dxx[:, :, None], dxy[:, :, None], dxy[:, :, None], dyy[:, :, None]),
         axis=2
     )
 
-    return H
+    return im_hess
