0abe2fdcc59a1e36f540e0e862516a38580b6f3a
Jonathan Beezley <jonathan.beezley@kitware.com>
2017-01-17 15:15:04 -0500

    Reuse router code from last version

--- web_client/router.js
+++ web_client/router.js
@@ -1,7 +1,49 @@
+import _ from 'underscore';
 import Backbone from 'backbone';
-// import router from 'girder/router';
+import { splitRoute, parseQueryString } from 'girder/misc';
 
-// unbind standard girder routes
-// Backbone.history.handlers = [];
+import events from './events';
 
-export default new Backbone.Router();
+var router = new Backbone.Router();
+
+router.setQuery = function setQuery(name, value, options) {
+    var curRoute = Backbone.history.fragment,
+        routeParts = splitRoute(curRoute),
+        queryString = parseQueryString(routeParts.name);
+    if (value === undefined || value === null) {
+        delete queryString[name];
+    } else {
+        queryString[name] = value;
+    }
+    var unparsedQueryString = $.param(queryString);
+    if (unparsedQueryString.length > 0) {
+        unparsedQueryString = '?' + unparsedQueryString;
+    }
+    this._lastQueryString = queryString;
+    this.navigate(routeParts.base + unparsedQueryString, options);
+};
+
+router.getQuery = function getQuery(name) {
+    return (this._lastQueryString || {})[name];
+};
+
+router.execute = function execute(callback, args) {
+    var query = parseQueryString(args.pop());
+    args.push(query);
+    if (callback) {
+        callback.apply(this, args);
+    }
+
+    _.each(this._lastQueryString || {}, function (value, key) {
+        if (!_.has(query, key)) {
+            events.trigger('query:' + key, null, query);
+        }
+    });
+    _.each(query, function (value, key) {
+        events.trigger('query:' + key, value, query);
+    });
+    events.trigger('query', query);
+    this._lastQueryString = query;
+};
+
+export default router;
