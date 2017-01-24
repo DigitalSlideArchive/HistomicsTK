15632a4a3813dc8183d7ec326071b985e9f2b1ea
cdeepakroy <cdeepakroy@gmail.com>
2017-01-24 10:16:42 +0530

    Use np.random.choice instead of np.random.uniform

--- histomicstk/utils/simple_mask.py
+++ histomicstk/utils/simple_mask.py
@@ -59,8 +59,8 @@ def simple_mask(im_rgb, bandwidth=2, bgnd_std=2.5, tissue_std=30,
     im_rgb = 255 * color.rgb2gray(im_rgb)
     im_rgb = im_rgb.astype(np.uint8)
     sI = im_rgb.flatten()[:, np.newaxis]
-    sI = sI[np.random.uniform(1, sI.size,
-                              (percent * im_rgb.size,)).astype(int)]
+    num_samples = np.int(percent * im_rgb.size)
+    sI = np.random.choice(sI, num_samples)
 
     # kernel-density smoothed histogram
     KDE = KernelDensity(kernel='gaussian', bandwidth=bandwidth).fit(sI)
