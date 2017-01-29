376910447755e71711381a2001ceefbf67c7715f
Jonathan Beezley <jonathan.beezley@kitware.com>
2017-01-29 11:53:06 -0500

    Fix routing

--- web_client/routes.js
+++ web_client/routes.js
@@ -1,15 +1,10 @@
 import events from './events';
-import FrontPageView from './views/body/FrontPageView';
 import ImageView from './views/body/ImageView';
 
 import Router from './router';
 
 function bindRoutes() {
     Router.route('', 'index', function () {
-        events.trigger('g:navigateTo', FrontPageView);
-    });
-
-    Router.route('image', 'image', function () {
         events.trigger('g:navigateTo', ImageView, {});
     });
     return Router;
