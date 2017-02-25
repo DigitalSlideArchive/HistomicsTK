0d11f170fd4e6199af50b85f13d02668d8c084e9
Sanghoon Lee <shlee8020@gmail.com>
2017-02-25 00:15:42 -0500

    Renamed vessless to membraneness

--- histomicstk/filters/shape/__init__.py
+++ histomicstk/filters/shape/__init__.py
@@ -7,7 +7,7 @@ shapes (e.g. blobs, vessels)
 # since we mostly have one function per file
 from .clog import clog
 from .glog import glog
-from .vesselness import vesselness
+from .membraneness import membraneness
 
 # list out things that are available for public use
 __all__ = (
@@ -15,5 +15,5 @@ __all__ = (
     # functions and classes of this package
     'clog',
     'glog',
-    'vesselness',
+    'membraneness',
 )
