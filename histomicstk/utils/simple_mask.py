2a957d44273ebc5a1bfd92551b1226d6964c9bc7
cdeepakroy <cdeepakroy@gmail.com>
2017-03-01 15:07:59 -0500

    Made magnification an optional parameter in sample_pixels and reinhard_stats

--- histomicstk/utils/simple_mask.py
+++ histomicstk/utils/simple_mask.py
@@ -56,10 +56,9 @@ def simple_mask(im_rgb, bandwidth=2, bgnd_std=2.5, tissue_std=30,
     """
 
     # convert image to grayscale, flatten and sample
-    im_rgb = 255 * color.rgb2gray(im_rgb)
-    im_rgb = im_rgb.astype(np.uint8)
+    im_input = color.rgb2gray(im_rgb).astype(np.uint8)
     num_samples = np.int(percent * im_rgb.size)
-    sI = np.random.choice(im_rgb.flatten(), num_samples)[:, np.newaxis]
+    sI = np.random.choice(im_input.flatten(), num_samples)[:, np.newaxis]
 
     # kernel-density smoothed histogram
     KDE = KernelDensity(kernel='gaussian', bandwidth=bandwidth).fit(sI)
