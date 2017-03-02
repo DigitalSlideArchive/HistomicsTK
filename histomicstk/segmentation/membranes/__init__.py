0de41e259b2a6061e620e075010c4b6225fa4595
Sanghoon Lee <shlee8020@gmail.com>
2017-03-02 13:56:49 -0500

    Added line to __init_.py

--- histomicstk/segmentation/membranes/__init__.py
+++ histomicstk/segmentation/membranes/__init__.py
@@ -1,10 +1,11 @@
 """
 This package contains implementations of state-of-th-art methods for
-segmenting nuclei from histopathology images.
+segmenting membranes from histopathology images.
 """
 
 from .membrane_detection import membrane_detection
 
 __all__ = (
-    'membrane_detection'
+
+    'membrane_detection',
 )
