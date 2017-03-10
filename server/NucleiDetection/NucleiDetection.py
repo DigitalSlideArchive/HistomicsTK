7341cc8aa6ce51c306b608678836761c58e71537
Jonathan Beezley <jonathan.beezley@kitware.com>
2017-03-10 14:20:40 -0500

    Add requried rotation to rectangle annotations

--- server/NucleiDetection/NucleiDetection.py
+++ server/NucleiDetection/NucleiDetection.py
@@ -112,6 +112,7 @@ def detect_tile_nuclei(slide_path, tile_position, args, **it_kwargs):
             "center":      [cx, cy, 0],
             "width":       width,
             "height":      height,
+            "rotation":    0
         }
 
         nuclei_bbox_list.append(cur_bbox)
