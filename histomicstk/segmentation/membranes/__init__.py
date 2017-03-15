411623bad6aec3162a03fd29b3a1eedbff31b93a
Sanghoon Lee <shlee8020@gmail.com>
2017-03-14 23:18:58 -0400

    Added membrane_neighbors.py to __init__.py

--- histomicstk/segmentation/membranes/__init__.py
+++ histomicstk/segmentation/membranes/__init__.py
@@ -4,8 +4,10 @@ segmenting membranes from histopathology images.
 """
 
 from .membrane_detection import membrane_detection
+from .membrane_neighbors import membrane_neighbors
 
 __all__ = (
 
     'membrane_detection',
+    'membrane_neighbors',
 )
