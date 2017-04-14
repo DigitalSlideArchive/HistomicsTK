f7062ed1156b66cd53d139313d3d78caf413d7e2
Neal Siekierski <neal.siekierski@kitware.com>
2017-04-14 14:34:47 -0400

    Default background_intensity magnification to 1.25

--- histomicstk/preprocessing/color_normalization/background_intensity.py
+++ histomicstk/preprocessing/color_normalization/background_intensity.py
@@ -7,11 +7,19 @@ def background_intensity(*args, **kwargs):
     are as in histomicstk.utils.sample_pixels, with background forced
     to True.
 
+    Notes
+    -----
+    The `magnification` parameter defaults to 1.25x, instead of the
+    native scan magnification as in sample_pixels.
+
     """
     if 'background' in kwargs:
         raise ValueError('"background" argument may not be specified')
 
     kwargs['background'] = True
+
+    if 'magnification' not in kwargs:
+        kwargs['magnification'] = 1.25
 
     sample = sample_pixels(*args, **kwargs)
 
