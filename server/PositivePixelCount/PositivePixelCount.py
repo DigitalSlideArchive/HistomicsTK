75854cab6a028c57296b67e5f781708e556afc16
Neal Siekierski <neal.siekierski@kitware.com>
2017-04-24 15:16:23 -0400

    Extract positive_pixel_count_single_tile

--- server/PositivePixelCount/PositivePixelCount.py
+++ server/PositivePixelCount/PositivePixelCount.py
@@ -39,20 +39,26 @@ def positive_pixel_count_tiles(args, kwargs, position, count):
     total = np.zeros(6) # In order: #weak, #+ve, #strong, sum_weak, sum_+ve, sum_strong
     for pos in range(position, position + count):
         tile = ts.getSingleTile(tile_position=pos, **kwargs)['tile'][..., :3] / 255.
-        tile_hsi = rgb_to_hsi(tile)
-        all_positive_i = tile_hsi[
-            (np.abs((tile_hsi[..., 0] - args.hueValue + 0.5 % 1) - 0.5) <= args.hueWidth / 2.)
-            & (tile_hsi[..., 1] >= args.saturationMinimum)
-            & (tile_hsi[..., 2] < args.intensityUpperLimit)
-            & (tile_hsi[..., 2] >= args.intensityLowerLimit)
-            , 2
-        ]
-        mask_weak = all_positive_i >= args.intensityWeakThreshold
-        total[[0, 3]] += np.count_nonzero(mask_weak), np.sum(all_positive_i[mask_weak])
-        mask_strong = all_positive_i < args.intensityStrongThreshold
-        total[[2, 5]] += np.count_nonzero(mask_strong), np.sum(all_positive_i[mask_strong])
-        mask_pos = ~(mask_weak | mask_strong)
-        total[[1, 4]] += np.count_nonzero(mask_pos), np.sum(all_positive_i[mask_pos])
+        total += positive_pixel_count_single_tile(args, tile)
+    return total
+
+
+def positive_pixel_count_single_tile(args, tile):
+    total = np.empty(6)
+    tile_hsi = rgb_to_hsi(tile)
+    all_positive_i = tile_hsi[
+        (np.abs((tile_hsi[..., 0] - args.hueValue + 0.5 % 1) - 0.5) <= args.hueWidth / 2.)
+        & (tile_hsi[..., 1] >= args.saturationMinimum)
+        & (tile_hsi[..., 2] < args.intensityUpperLimit)
+        & (tile_hsi[..., 2] >= args.intensityLowerLimit)
+        , 2
+    ]
+    mask_weak = all_positive_i >= args.intensityWeakThreshold
+    total[[0, 3]] = np.count_nonzero(mask_weak), np.sum(all_positive_i[mask_weak])
+    mask_strong = all_positive_i < args.intensityStrongThreshold
+    total[[2, 5]] = np.count_nonzero(mask_strong), np.sum(all_positive_i[mask_strong])
+    mask_pos = ~(mask_weak | mask_strong)
+    total[[1, 4]] = np.count_nonzero(mask_pos), np.sum(all_positive_i[mask_pos])
     return total
 
 
