615a2718f0d62205869b72e347cf909c8c43c4de
Jonathan Beezley <jonathan.beezley@kitware.com>
2017-04-26 14:56:31 -0400

    Add docstrings to the popover view

--- web_client/views/popover/AnnotationPopover.js
+++ web_client/views/popover/AnnotationPopover.js
@@ -7,13 +7,22 @@ import ElementCollection from 'girder_plugins/large_image/collections/ElementCol
 import annotationPopover from '../../templates/popover/annotationPopover.pug';
 import '../../stylesheets/popover/annotationPopover.styl';
 
+/**
+ * This view behaves like a bootstrap "popover" that follows the mouse pointer
+ * over the image canvas and dynamically updates according to the features
+ * under the pointer.
+ *
+ * @param {object} [settings]
+ * @param {number} [settings.debounce]
+ *   Debounce time in ms for rerendering due to mouse movements
+ */
 var AnnotationPopover = View.extend({
     initialize(settings) {
         if (settings.debounce) {
-            this.position = _.debounce(this.position, settings.debounce);
+            this._position = _.debounce(this._position, settings.debounce);
         }
 
-        $('body').on('mousemove', '.h-image-view-body', (evt) => this.position(evt));
+        $('body').on('mousemove', '.h-image-view-body', (evt) => this._position(evt));
         $('body').on('mouseout', '.h-image-view-body', () => this._hide());
         $('body').on('mouseover', '.h-image-view-body', () => this._show());
 
@@ -41,6 +50,32 @@ var AnnotationPopover = View.extend({
         this._height = this.$('.h-annotation-popover').height();
     },
 
+    /**
+     * Set the popover visibility state.
+     *
+     * @param {boolean} [show]
+     *   if true: show the popover
+     *   if false: hide the popover
+     *   if undefined: toggle the popover state
+     */
+    toggle(show) {
+        if (show === undefined) {
+            show = this._hidden;
+        }
+        this._hidden = !show;
+        this.render();
+        return this;
+    },
+
+    /**
+     * Check the local cache for the given creator.  If it has not already been
+     * fetched, then send a rest request to get the user information and
+     * rerender the popover.
+     *
+     * As a consequence to avoid always rendering asyncronously, the user name
+     * will not be shown on the first render.  In practice, this isn't usually
+     * noticeable.
+     */
     _getUser(model) {
         var id = model.get('annotation').get('creatorId');
         if (!_.has(this._users, id)) {
@@ -53,37 +88,51 @@ var AnnotationPopover = View.extend({
         }
     },
 
+    /**
+     * Format a Date object as a localized string.
+     */
     _formatDate(s) {
         var d = new Date(s);
         return d.toLocaleString();
     },
 
+    /**
+     * Remove the hidden class on the popover element if this._visible()
+     * returns true.
+     */
     _show() {
         if (this._visible()) {
             this.$el.removeClass('hidden');
         }
     },
 
+    /**
+     * Unconditionally hide popover.
+     */
     _hide() {
         this.$el.addClass('hidden');
     },
 
+    /**
+     * Determine if the popover should be visible.  Returns true
+     * if there are active annotations under the mouse pointer and
+     * the label option is enabled.
+     */
     _visible() {
         return !this._hidden && this.collection.length > 0;
     },
 
-    position(evt) {
+    /**
+     * Reset the position of the popover to the position of the
+     * mouse pointer.
+     */
+    _position(evt) {
         if (this._visible()) {
             this.$el.css({
                 left: evt.pageX + 5,
                 top: evt.pageY - this._height / 2
             });
         }
-    },
-
-    toggle(show) {
-        this._hidden = !show;
-        this.render();
     }
 });
 
