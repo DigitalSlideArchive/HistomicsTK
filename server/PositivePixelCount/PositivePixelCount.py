a249434d741fc6413a0efbb5ab7fe1482c1460dc
Neal Siekierski <neal.siekierski@kitware.com>
2017-05-01 18:22:31 -0400

    Use dicts instead of index-by-number numpy arrays

--- server/PositivePixelCount/PositivePixelCount.py
+++ server/PositivePixelCount/PositivePixelCount.py
@@ -33,34 +33,36 @@ def main(args):
                 args, kwargs, position, min(args.tile_grouping, total_tiles - position)))
         results = delayed(combine)(results).compute()
 
-    total_all_positive = results[:3].sum()
-    output = (zip(['NumberWeakPositive', 'NumberPositive', 'NumberStrongPositive',
-                   'IntensitySumWeakPositive', 'IntensitySumPositive', 'IntensitySumStrongPositive'],
-                  results)
-              + [('IntensityAverage', results[3:].sum() / total_all_positive),
-                 ('RatioStrongToTotal', results[2] / total_all_positive),
-                 ('IntensityAverageWeakAndPositive', results[3:5].sum() / results[:2].sum())])
+    r = results
+    total_all_positive = sum(r[k] for k in results_num_keys)
+    output = ([(k, r[k]) for k in results_keys]
+              + [('IntensityAverage', sum(r[k] for k in results_i_keys) / total_all_positive),
+                 ('RatioStrongToTotal', float(r['NumberStrongPositive']) / total_all_positive),
+                 ('IntensityAverageWeakAndPositive',
+                  (r['IntensitySumWeakPositive'] + r['IntensitySumPositive'])
+                  / (r['NumberWeakPositive'] + r['NumberPositive']))])
     with open(args.returnParameterFile, 'w') as f:
         for k, v in output:
             f.write('{} = {}\n'.format(k, v))
 
 
 def combine(results):
-    return np.sum(results, 0) if results else np.zeros(6)
+    return {k: sum(r[k] for r in results) for k in results_keys}
 
 
 def positive_pixel_count_tiles(args, kwargs, position, count):
     ts = large_image.getTileSource(args.inputImageFile)
-    total = np.zeros(6) # In order: #weak, #+ve, #strong, sum_weak, sum_+ve, sum_strong
+    total = dict((k, 0) for k in results_keys)
     for pos in range(position, position + count):
         tile = ts.getSingleTile(tile_position=pos, **kwargs)['tile']
-        total += positive_pixel_count_single_tile(args, tile, makeLabelImage=False)
+        subtotal = positive_pixel_count_single_tile(args, tile, makeLabelImage=False)
+        for k in results_keys:
+            total[k] += subtotal[k]
     return total
 
 
 def positive_pixel_count_single_tile(args, tile, makeLabelImage):
     tile = tile[..., :3]
-    total = np.empty(6)
     tile_hsi = rgb_to_hsi(tile / 255.)
     mask_all_positive = (
         (np.abs((tile_hsi[..., 0] - args.hueValue + 0.5 % 1) - 0.5) <= args.hueWidth / 2.)
@@ -70,11 +72,19 @@ def positive_pixel_count_single_tile(args, tile, makeLabelImage):
     )
     all_positive_i = tile_hsi[mask_all_positive, 2]
     mask_weak = all_positive_i >= args.intensityWeakThreshold
-    total[[0, 3]] = np.count_nonzero(mask_weak), np.sum(all_positive_i[mask_weak])
+    nw, iw = np.count_nonzero(mask_weak), np.sum(all_positive_i[mask_weak])
     mask_strong = all_positive_i < args.intensityStrongThreshold
-    total[[2, 5]] = np.count_nonzero(mask_strong), np.sum(all_positive_i[mask_strong])
+    ns, is_ = np.count_nonzero(mask_strong), np.sum(all_positive_i[mask_strong])
     mask_pos = ~(mask_weak | mask_strong)
-    total[[1, 4]] = np.count_nonzero(mask_pos), np.sum(all_positive_i[mask_pos])
+    np_, ip = np.count_nonzero(mask_pos), np.sum(all_positive_i[mask_pos])
+    total = dict(
+        NumberWeakPositive=nw,
+        NumberPositive=np_,
+        NumberStrongPositive=ns,
+        IntensitySumWeakPositive=iw,
+        IntensitySumPositive=ip,
+        IntensitySumStrongPositive=is_,
+    )
     if makeLabelImage:
         labelImage = np.full_like(tile, 255)
         # Colors from the "coolwarm" color map
@@ -98,6 +108,10 @@ def rgb_to_hsi(im):
     saturations = np.where(intensities, 1 - im.min(0) / intensities, 0)
     return np.stack([hues, saturations, intensities], -1)
 
+
+results_num_keys = 'NumberWeakPositive', 'NumberPositive', 'NumberStrongPositive'
+results_i_keys = 'IntensitySumWeakPositive', 'IntensitySumPositive', 'IntensitySumStrongPositive'
+results_keys = results_num_keys + results_i_keys
 
 if __name__ == '__main__':
     main(CLIArgumentParser().parse_args())
