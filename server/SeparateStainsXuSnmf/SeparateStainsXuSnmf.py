b265358c13caac653fab117cffd1eef73cc7b124
Neal Siekierski <neal.siekierski@kitware.com>
2017-07-10 11:34:16 -0400

    Replace SparseColorDeconvolution with SeparateStainsXuSnmf (#336)
    Since #280, `sparse_color_deconvolution` in `histomicstk.preprocessing.color_deconvolution` has been redundant with a combination of `separate_stains_xu_snmf` and `color_deconvolution`. This PR replaces this routine and the corresponding CLI, `SparseColorDeconvolution`, with routines and CLIs mirroring those around the PCA-based Macenko method.
    
    The import changes made are namely:
    - removing `sparse_color_deconvolution`,
    - creating a new routine `rgb_separate_stains_xu_snmf` mirroring `rgb_separate_stains_macenko_pca`,
    - converting `SparseColorDeconvolution` from a wrapper around `sparse_color_deconvolution` to `SeparateStainsXuSnmf`, a wrapper around `rgb_separate_stains_xu_snmf`, and
    - teaching `SeparateStainsXuSnmf` the same extra parameters as `SeparateStainsMacenkoPCA`, namely ones for pixel sampling and Dask configuration.
    
    This PR also makes other CLIs make use of `cli_common.utils.create_dask_client`, introduced by #331, and gives this routine a docstring.
    
    Commit history:
    * Define rgb_separate_stains_xu_snmf
    
    * Change SparseColorDeconvolution to just compute the stain matrix
    
    * Remove sparse_color_deconvolution
    
    * Rename SparseColorDeconvolution to SeparateStainsXuSnmf
    
    * Add modified "splitArgs" to cli_common.utils
    
    * Use new splitArgs in SeparateStainsMacenkoPCA
    
    * Extra parameters for SeparateStainsXuSnmf
    
    * Fix bad use around w_est
    
    * Factor out utils.sample_pixels
    
    * Collect Dask-starting logic in cli_common
    
    * Give create_dask_client a docstring
    
    * Remove start_dask; use create_dask_client instead
    
    * Use argparse's Namespace instead of our own
    
    This cleans up existing code a little and also will automatically
    enable extra operations such as equality comparisons.
    
    * Fix intermittent failures in cli_common_test
    
    histomicstk.utils.simple_mask gives slightly different output
    from run to run, probably due to its use of randomness.
    
    * Add test for cli_common.utils.splitArgs

---
+++ server/SeparateStainsXuSnmf/SeparateStainsXuSnmf.py
@@ -0,0 +1,41 @@
+import os
+import sys
+
+import numpy
+
+from ctk_cli import CLIArgumentParser
+
+import histomicstk.preprocessing.color_deconvolution as htk_cdeconv
+
+sys.path.append(os.path.normpath(os.path.join(os.path.dirname(__file__), '..')))
+from cli_common import utils  # noqa
+
+
+def main(args):
+    args = utils.splitArgs(args)
+    args.snmf.I_0 = numpy.array(args.snmf.I_0)
+
+    print(">> Starting Dask cluster and sampling pixels")
+    utils.create_dask_client(args.dask)
+    sample = utils.sample_pixels(args.sample)
+
+    # Create stain matrix
+    print('>> Creating stain matrix')
+
+    args.snmf.w_init = utils.get_stain_matrix(args.stains, 2)
+
+    print args.snmf.w_init
+
+    # Perform color deconvolution
+    print('>> Performing color deconvolution')
+
+    w_est = htk_cdeconv.rgb_separate_stains_xu_snmf(sample.T, **vars(args.snmf))
+    w_est = htk_cdeconv.complement_stain_matrix(w_est)
+
+    with open(args.returnParameterFile, 'w') as f:
+        for i, stain in enumerate(w_est.T):
+            f.write('stainColor_{} = {}\n'.format(i+1, ','.join(map(str, stain))))
+
+
+if __name__ == "__main__":
+    main(CLIArgumentParser().parse_args())
