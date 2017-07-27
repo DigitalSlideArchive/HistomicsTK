b2a305980871373281e7da518b29077d00fd45d9
Jonathan Beezley <jonathan.beezley@kitware.com>
2017-07-27 13:40:41 -0400

    Replace analysis dropdown menu with a pagination widget

--- web_client/dialogs/index.js
+++ web_client/dialogs/index.js
@@ -1,11 +1,13 @@
 import openAnnotatedImage from './openAnnotatedImage';
 import openImage from './openImage';
+import openAnalysis from './openAnalysis';
 import editAnnotation from './editAnnotation';
 import saveAnnotation from './saveAnnotation';
 
 export {
     openAnnotatedImage,
     openImage,
+    openAnalysis,
     editAnnotation,
     saveAnnotation
 };
