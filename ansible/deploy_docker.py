8aa22f5efa0d53f89f6b5dd5b2ebbaa28f1e1461
David Manthey <david.manthey@kitware.com>
2017-08-01 16:09:11 -0400

    Move getting the docker client to a function.

--- ansible/deploy_docker.py
+++ ansible/deploy_docker.py
@@ -74,8 +74,7 @@ def containers_provision(**kwargs):
     """
     Provision or reprovision the containers.
     """
-    client = docker.from_env(version='auto')
-    client = client if not hasattr(client, 'api') else client.api
+    client = docker_client()
     ctn = get_docker_image_and_container(
         client, 'histomicstk', version=kwargs.get('pinned'))
 
@@ -145,8 +144,7 @@ def containers_start(port=8080, rmq='docker', mongo='docker', provision=False,
     :param provision: if True, reprovision after starting.  Otherwise, only
         provision if the histomictk container is created.
     """
-    client = docker.from_env(version='auto')
-    client = client if not hasattr(client, 'api') else client.api
+    client = docker_client()
     env = {}
     started = False
 
@@ -385,8 +383,7 @@ def containers_status(**kwargs):
     """"
     Report the status of any containers we are responsible for.
     """
-    client = docker.from_env(version='auto')
-    client = client if not hasattr(client, 'api') else client.api
+    client = docker_client()
 
     keys = ImageList.keys()
     results = []
@@ -415,8 +412,7 @@ def containers_stop(remove=False, **kwargs):
 
     :param remove: True to remove the containers.  False to just stop them.
     """
-    client = docker.from_env(version='auto')
-    client = client if not hasattr(client, 'api') else client.api
+    client = docker_client()
     keys = ImageList.keys()
     keys.reverse()
     for key in keys:
@@ -431,6 +427,16 @@ def containers_stop(remove=False, **kwargs):
 
     if remove:
         network_remove(client, BaseName)
+
+
+def docker_client():
+    """
+    Return the current docker client in a manner that works with both the
+    docker-py and docker modules.
+    """
+    client = docker.from_env(version='auto')
+    client = client if not hasattr(client, 'api') else client.api
+    return client
 
 
 def docker_mounts():
@@ -517,8 +523,7 @@ def images_build(retry=False, names=None):
         names to build.
     """
     basepath = os.path.dirname(os.path.realpath(__file__))
-    client = docker.from_env(version='auto')
-    client = client if not hasattr(client, 'api') else client.api
+    client = docker_client()
 
     if names is None:
         names = ImageList.keys()
@@ -559,8 +564,7 @@ def images_repull(**kwargs):
     """"
     Repull all docker images.
     """
-    client = docker.from_env(version='auto')
-    client = client if not hasattr(client, 'api') else client.api
+    client = docker_client()
     for key, image in six.iteritems(ImageList):
         if 'name' not in image and not kwargs.get('cli'):
             continue
