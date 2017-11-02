57445f7b2a330a1c81df0efc274aa3304b21de3a
Deepak Chittajallu <cdeepakroy@gmail.com>
2017-11-02 10:52:31 -0400

    Added echo to stderr notifying GPU is available

--- server/docker-entrypoint.sh
+++ server/docker-entrypoint.sh
@@ -3,6 +3,7 @@
 if type nvidia-smi >/dev/null 2>/dev/null;
 then
     source /venv-gpu/bin/activate
+    echo "NOTE: GPU available" >&2
 fi
 
 python /build/slicer_cli_web/server/cli_list_entrypoint.py "$@"
