a1b1f5615582c3de075e16c8cf218f94584186e2
Deepak Chittajallu <cdeepakroy@gmail.com>
2017-11-02 10:31:42 -0400

    Fix bugs in docker-entrypoint.sh

--- server/docker-entrypoint.sh
+++ server/docker-entrypoint.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-if type nvidia-smi > /dev/null;
+if type nvidia-smi >/dev/null 2>/dev/null;
 then
     source /venv-gpu/bin/activate
 fi
 
-python /build/slicer_cli_web/server/cli_list_entrypoint.py
+python /build/slicer_cli_web/server/cli_list_entrypoint.py "$@"
