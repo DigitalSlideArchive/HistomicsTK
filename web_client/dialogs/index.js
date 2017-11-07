5fc91224fc13087b6818c7a929a84a5b2c56a21d
Pierre Assemat <pierre.assemat@kitware.com>
2017-11-07 13:26:58 -0500

    Client: Create 'editRegionOfInterest' dialog view
    Convert the file's size and disable the download
    if the size is higher than 1GB.

--- web_client/dialogs/index.js
+++ web_client/dialogs/index.js
@@ -3,11 +3,13 @@ import openAnnotatedImage from './openAnnotatedImage';
 import openImage from './openImage';
 import editAnnotation from './editAnnotation';
 import saveAnnotation from './saveAnnotation';
+import editRegionOfInterest from './editRegionOfInterest';
 
 export {
     confirmDialog,
     openAnnotatedImage,
     openImage,
     editAnnotation,
-    saveAnnotation
+    saveAnnotation,
+    editRegionOfInterest
 };
