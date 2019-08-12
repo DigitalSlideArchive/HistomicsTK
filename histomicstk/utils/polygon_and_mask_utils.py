c1281ba0c6dcc55b0cb89443442d72b4533f1663
kheffah <mtageld@emory.edu>
2019-08-12 01:33:53 -0400

    [WIP] - utils for parsing mask from polygonal annotations from slide

--- histomicstk/utils/polygon_and_mask_utils.py
+++ histomicstk/utils/polygon_and_mask_utils.py
@@ -15,8 +15,10 @@ from pandas import DataFrame
 
 def get_image_from_htk_response(resp):
     """Given a histomicsTK girder response, get np array image.
+    
     Arguments:
         * resp - response from server request
+    
     Returns:
         * a pillow Image object
         
@@ -42,10 +44,12 @@ def rotate_point_list(point_list, rotation, center=(0, 0)):
     Modified from javascript version at:
         https://github.com/girder/large_image/blob/master/ ...
         web_client/annotations/rotate.js
+    
     Arguments:
         * point_list - list of tuples (x, y)
         * rotation - degrees (in radians) 
         * center - central point coordinates
+    
     Returns:
         * list of tuples 
     """
@@ -55,7 +59,6 @@ def rotate_point_list(point_list, rotation, center=(0, 0)):
     
         cos = np.cos(rotation)
         sin = np.sin(rotation)
-        
         x = point[0] - center[0]
         y = point[1] - center[1]
         
@@ -71,11 +74,13 @@ def get_rectangular_roi_coords(
         roi_center, roi_width, roi_height, roi_rotation=0):
     """ Given data on rectangular ROI center/width/height/rotation, 
     get the unrotated abounding box coordinates around rotated ROI    
+    
     Arguments:
-        roi_center - [x, y] format
-        roi_width, roi_height, roi_rotation - float
+        * roi_center - [x, y] format
+        * roi_width, roi_height, roi_rotation - float
+    
     Returns:
-        dict with roi corners and bounds
+        * dict with roi corners and bounds
     """
     # Get false bounds
     x_min = roi_center[0] - int(roi_width/2)
@@ -105,8 +110,10 @@ def get_rectangular_roi_coords(
 
 def get_bboxes_from_slide_annotations(slide_annotations):
     """Given a slide annotation list, gets information on bounding boxes
+    
     Arguments:
         * slide_annotations - response from server request (list of dicts)
+    
     Returns:
         * A pandas DataFrame. The columns annidx and elementidx encode the
           dict index of annotation document and element, respectively, in the 
@@ -164,6 +171,63 @@ def get_bboxes_from_slide_annotations(slide_annotations):
     return element_infos
 
 #%% ===========================================================================
+
+def np_vec_no_jit_iou(bboxes1, bboxes2):
+    """ Fast, vectorized IoU.
+    Source: https://medium.com/@venuktan/vectorized-intersection-over-union ...
+            -iou-in-numpy-and-tensor-flow-4fa16231b63d
+    
+    Arguments:
+        * bboxes1 - np array where columns encode bounding box 
+                  corners xmin, ymin, xmax, ymax
+        * bboxes2 - same as bboxes 1
+    
+    Returns:
+        * np array of IoU values for each pair from bboxes1 & bboxes2
+    """
+    x11, y11, x12, y12 = np.split(bboxes1, 4, axis=1)
+    x21, y21, x22, y22 = np.split(bboxes2, 4, axis=1)
+    xA = np.maximum(x11, np.transpose(x21))
+    yA = np.maximum(y11, np.transpose(y21))
+    xB = np.minimum(x12, np.transpose(x22))
+    yB = np.minimum(y12, np.transpose(y22))
+    interArea = np.maximum((xB - xA + 1), 0) * np.maximum((yB - yA + 1), 0)
+    boxAArea = (x12 - x11 + 1) * (y12 - y11 + 1)
+    boxBArea = (x22 - x21 + 1) * (y22 - y21 + 1)
+    iou = interArea / (boxAArea + np.transpose(boxBArea) - interArea)
+    return iou
+
+#%% ===========================================================================
+
+def get_idxs_for_annots_within_roi_by_bbox(
+        element_infos, idx_for_roi, iou_thresh=0.0):
+    """Find indices of **potentially** included annoations within the ROI
+    We say "potentially" because this uses the IoU of roi and annotation 
+    as a fast indicator of potential inclusion. This helps dramatically scale 
+    down the number of annotations to look through. Later on, a detailed look
+    at wheter the annotation polygons actually overlap the ROI can be done.
+    
+    Arguments:
+        * element_infos - pandas DataFrame 
+               from running get_bboxes_from_slide_annotations()
+        * idx_for_roi - index for roi annotation within the element_infos DF
+        * iou_thresh - overlap threshold to be considered within ROI
+    
+    Returns:
+        * list of indices elative to element_infos
+    """
+    bboxes = np.array(
+            element_infos.loc[:, ['xmin', 'ymin', 'xmax', 'ymax']], 
+            dtype='int')
+    iou = np_vec_no_jit_iou(bboxes[idx_for_roi, :][None,...], bboxes2=bboxes)
+    iou = np.concatenate((np.arange(iou.shape[1])[None, ...], iou))
+    iou = iou[:, iou[1, :] > iou_thresh]    
+    
+    overlaps = set([int(j) for j in iou[0, :]]) - {idx_for_roi}
+    
+    return list(overlaps)
+
+#%% ===========================================================================
     
 #import girder_client 
 #APIURL = 'http://demo.kitware.com/histomicstk/api/v1/'
@@ -176,11 +240,15 @@ def get_bboxes_from_slide_annotations(slide_annotations):
 ## get annotations for slide
 #slide_annotations = gc.get('/annotation/item/' + SAMPLE_SLIDE_ID)
 #    
-##%%
-#
+## get bounding box information for all annotations
 #element_infos = get_bboxes_from_slide_annotations(slide_annotations)
 #
-
+## get indices of annotations that overlap (belong to) ROI
+#idx_for_roi = 0 # <- inspect the element_infos DF!!
+#overlaps = get_idxs_for_annots_within_roi_by_bbox(
+#        element_infos, idx_for_roi=0, iou_thresh=0.0)
+#
+##%%
 
 
 
