439de744c3b59a606ce684cbf1f1018a82dfde29
kheffah <mtageld@emory.edu>
2019-08-12 21:49:56 -0400

    fix import for annots and masks handler

--- histomicstk/annotations_and_masks/annotations_to_masks_handler.py
+++ histomicstk/annotations_and_masks/annotations_to_masks_handler.py
@@ -10,8 +10,7 @@ import numpy as np
 from pandas import DataFrame, read_csv
 from imageio import imwrite
 
-#from histomicstk.annotations_and_masks.annotation_and_mask_utils import (
-from annotation_and_mask_utils import (
+from histomicstk.annotations_and_masks.annotation_and_mask_utils import (
     get_bboxes_from_slide_annotations, _get_idxs_for_all_rois, 
     get_idxs_for_annots_overlapping_roi_by_bbox, _get_element_mask, 
     _add_element_to_roi)
