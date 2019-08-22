047c4c674559b20a55cd606ef0540142d47204a5
kheffah <mtageld@emory.edu>
2019-08-22 18:32:29 +0000

    [WIP] - use normal python tests as opposed to server side tests

--- plugin_tests/annotations_to_masks_handler_test.py
+++ plugin_tests/annotations_to_masks_handler_test.py
@@ -32,43 +32,8 @@ def tearDownModule():
     base.stopServer()
 
 
-# %%===========================================================================
-# Constants & prep work
-# =============================================================================
-
-
 class GetROIMasksTest(GirderClientTestCase):
     """Test methods for getting ROI mask from annotations."""
-
-    def test_get_roi_mask(self):
-        """Test get_roi_mask()."""
-        slide_annotations = self.gc.get('/annotation/item/' + self.wsiFile['itemId'])
-        element_infos = get_bboxes_from_slide_annotations(slide_annotations)
-
-        # read ground truth codes and information
-        GTCodes = read_csv(GTCODE_PATH)
-        GTCodes.index = GTCodes.loc[:, 'group']
-
-        # get indices of rois
-        idxs_for_all_rois = _get_idxs_for_all_rois(
-            GTCodes=GTCodes, element_infos=element_infos)
-
-        # get roi mask and info
-        ROI, roiinfo = get_roi_mask(
-            slide_annotations=slide_annotations, element_infos=element_infos,
-            GTCodes_df=GTCodes.copy(),
-            idx_for_roi=idxs_for_all_rois[0],  # <- let's focus on first ROI,
-            iou_thresh=0.0, roiinfo=None, crop_to_roi=True, use_shapely=True,
-            verbose=False, monitorPrefix="roi 1")
-
-        self.assertTupleEqual(ROI.shape, (4594, 4542))
-        self.assertTupleEqual((
-            roiinfo['BBOX_HEIGHT'], roiinfo['BBOX_WIDTH'],
-            roiinfo['XMIN'], roiinfo['XMAX'],
-            roiinfo['YMIN'], roiinfo['YMAX']),
-            (4595, 4543, 59206, 63749, 33505, 38100))
-
-    # %% ----------------------------------------------------------------------
 
     def test_get_all_roi_masks_for_slide(self):
         """Test get_all_roi_masks_for_slide()."""
