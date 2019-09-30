b53f169b78f1872eb8c0f760a86d3856a46f4b67
kheffah <mtageld@emory.edu>
2019-09-30 22:26:37 +0000

    add slide iterator

--- histomicstk/workflows/tests/workflow_runner_test.py
+++ histomicstk/workflows/tests/workflow_runner_test.py
@@ -5,38 +5,69 @@ Created on Mon Sep 30 18:12:48 2019
 
 @author: mtageld
 """
+import unittest
 import tempfile
 import girder_client
 import numpy as np
+from histomicstk.workflows.workflow_runner import Slide_iterator
 
 # %%===========================================================================
 # Constants & prep work
 
 APIURL = 'http://candygram.neurology.emory.edu:8080/api/v1/'
-SAMPLE_SLIDE_ID = "5d586d76bd4404c6b1f286ae"
-# SAMPLE_SLIDE_ID = "5d8c296cbd4404c6b1fa5572"
+SAMPLE_SOURCE_FOLDER_ID = "5d5c28c6bd4404c6b1f3d598"
 
 gc = girder_client.GirderClient(apiUrl=APIURL)
 gc.authenticate(apiKey='kri19nTIGOkWH01TbzRqfohaaDWb6kPecRqGmemb')
 
 logging_savepath = tempfile.mkdtemp()
 
-# from the TCGA-A2-A3XS-DX1 ROI in Amgad et al, 2019
-cnorm_main = {
-    'mu': np.array([8.74108109, -0.12440419,  0.0444982]),
-    'sigma': np.array([0.6135447, 0.10989545, 0.0286032]),
-}
+## from the TCGA-A2-A3XS-DX1 ROI in Amgad et al, 2019
+#cnorm_main = {
+#    'mu': np.array([8.74108109, -0.12440419,  0.0444982]),
+#    'sigma': np.array([0.6135447, 0.10989545, 0.0286032]),
+#}
 
 # %%===========================================================================
 
-# Params for slide iterator
-source_folder_id = "5d5c28c6bd4404c6b1f3d598"
-keep_slides = None
-discard_slides = None
-# gc
+class Slide_iterator_Test(unittest.TestCase):
+    """Test slide iterator."""
 
-# Params for workflow runner
-# destin_folder_id = "5d9246f6bd4404c6b1faaa89"
+    def test_Slide_iterator(self):
+        """Test Slide_iterator.run()."""
+        si = Slide_iterator(gc, source_folder_id=SAMPLE_SOURCE_FOLDER_ID)
+
+        self.assertGreaterEqual(len(si.slide_ids), 1)
+
+        sir = si.run()
+        fetched = []
+        for i in range(len(si.slide_ids)):
+            sname, sid = next(sir)
+            fetched.append(sname)
+
+        self.assertTupleEqual(tuple(si.slide_ids.keys()), tuple(fetched))
+
 
 # %%===========================================================================
 
+
+if __name__ == '__main__':
+    unittest.main()
+
+
+
+
+# %%===========================================================================
+# %%===========================================================================
+# %%===========================================================================
+
+#from histomicstk.utils.general_utils import Base_HTK_Class
+#
+## Params for workflow runner
+## destin_folder_id = "5d9246f6bd4404c6b1faaa89"
+#
+## %%===========================================================================
+#
+#
+#
+#
---
+++ histomicstk/workflows/workflow_runner.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 30 22:09:40 2019
+
+@author: mtageld
+"""
+from histomicstk.utils.general_utils import Base_HTK_Class
+
+# %% ==========================================================================
+
+
+class Slide_iterator(Base_HTK_Class):
+    """Iterate through large_image items in a girder folder."""
+
+    def __init__(self, gc, source_folder_id, **kwargs):
+        """Init Cellularity_Detector_Superpixels object.
+
+        Arguments:
+        -----------
+        gc : object
+            girder client object
+        source_folder_id : str
+            girder ID of folder in which slides are located
+        keep_slides : list
+            List of slide names to keep. If None, all are kept.
+        discard_slides : list
+            List of slide names to discard.
+        kwargs : key-value pairs
+            The following are already assigned defaults by Base_HTK_Class
+            but can be passed here to override defaults
+            [verbose, monitorPrefix, logger, logging_savepath,
+            suppress_warnings]
+
+        """
+        default_attr = {
+            'keep_slides': None,
+            'discard_slides': [],
+        }
+        default_attr.update(kwargs)
+        super(Slide_iterator, self).__init__(default_attr=default_attr)
+
+        # set attribs
+        self.gc = gc
+        self.source_folder_id = source_folder_id
+        self.set_slide_ids()
+
+    # =========================================================================
+
+    def set_slide_ids(self):
+        """Get dict of slide idx, indexed by name."""
+        resp = self.gc.get("item?folderId=%s" % self.source_folder_id)
+        self.slide_ids = {j['name']: j['_id'] for j in resp}
+        # find discard ids
+        if self.keep_slides is not None:
+            discard = set(self.slide_ids.keys()) - set(self.keep_slides)
+            self.discard_slides.extend(list(discard))
+            self.discard_slides = list(set(self.discard_slides))
+        # only keep what's relevant
+        for sn in self.discard_slides:
+            del self.slide_ids[sn]
+
+    # =========================================================================
+
+    def run(self):
+        """Yields information on one slide at a time."""
+        for sname, sid in self.slide_ids.items():
+            yield sname, sid
+
+# %% ==========================================================================
