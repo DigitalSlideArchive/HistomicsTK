330a5cbf2fc0deb6cd34d1317a92658d419797e8
kheffah <mtageld@emory.edu>
2020-01-20 21:37:26 -0500

    Revert "Merge branch 'mtageld-dev' into mongo-to-sqlite"
    This reverts commit 706096fe890f46dd4b7acfc40d965e7f105ae2ea, reversing
    changes made to 3b59dede3ae6bc723bb01c3c7d5c8547ffc0de7c.

--- histomicstk/utils/tests/girder_convenience_utils_test.py
+++ histomicstk/utils/tests/girder_convenience_utils_test.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import unittest
+
 from histomicstk.utils.girder_convenience_utils import connect_to_api
 
 # %%===========================================================================
