deb5b88aab697dd0f424ac3d5b1a7015eac33d9c
kheffah <mtageld@emory.edu>
2020-02-07 16:54:17 -0500

    unit test written. Docs left abd then will be ready to merge

--- histomicstk/annotations_and_masks/tests/review_gallery_test.py
+++ histomicstk/annotations_and_masks/tests/review_gallery_test.py
@@ -1,14 +1,10 @@
-import unittest
-
 import os
 import tempfile
 import shutil
 import unittest
 
-import matplotlib.pylab as plt
 from imageio import imread
 from pandas import read_csv
-import numpy as np
 import girder_client
 from histomicstk.annotations_and_masks.review_gallery import \
     get_all_rois_from_folder_v2, _plot_rapid_review_vis,\
@@ -57,6 +53,7 @@ class ReviewGallery(unittest.TestCase):
 
     def test_get_all_rois_from_folder_v2(self):
         """Test get_all_rois_from_folder_v2()."""
+        print("Test get_all_rois_from_folder_v2()")
 
         # params for getting all rois for slide
         get_all_rois_kwargs = {
@@ -85,8 +82,40 @@ class ReviewGallery(unittest.TestCase):
             gc=gc, folderid=SAMPLE_FOLDER_ID,
             get_all_rois_kwargs=get_all_rois_kwargs, monitor='test')
 
+        # a couple of checks
+        all_fovs = os.listdir(combinedvis_savepath)
+        fovdict = dict()
+        for fov in all_fovs:
+            slide = fov[:23]
+            if slide not in fovdict.keys():
+                fovdict[slide] = []
+            fovdict[slide].append(fov.split(slide)[1].split('.png')[0])
+
+        self.assertSetEqual(
+            set(fovdict.keys()),
+            {'TCGA-A1-A0SP-01Z-00-DX1', 'TCGA-A7-A0DA-01Z-00-DX1'})
+
+        self.assertSetEqual(
+            set(fovdict['TCGA-A1-A0SP-01Z-00-DX1']),
+            {'_id-5e2a2d77ddda5f83986d135b_left-%d_top-%d_bottom-%d_right-%d'
+                % (l, t, b, r) for (l, t, b, r) in [
+                  (10124, 56533, 56789, 10380), (8076, 56021, 56277, 8332),
+                  (8332, 54485, 54741, 8588), (8076, 57045, 57301, 8332),
+                  (9356, 53973, 54229, 9612), (8076, 55509, 55765, 8332),
+                  (7308, 55253, 55509, 7564), (9100, 55765, 56021, 9356),
+                  (9356, 55765, 56021, 9612), (8332, 57045, 57301, 8588),
+                  (8844, 57301, 57557, 9100)]}
+        )
+
+        # shape & value check
+        imname = 'TCGA-A1-A0SP-01Z-00-DX1_id-5e2a2d77ddda5f83986d135b' \
+            + '_left-10124_top-56533_bottom-56789_right-10380'
+        cvis = imread(os.path.join(combinedvis_savepath, imname + '.png'))
+        self.assertTupleEqual(cvis.shape, (403, 1209, 3))
+
     def test_create_review_galleries(self):
         """Test create_review_galleries()."""
+        print("Test create_review_galleries()")
 
         create_review_galleries_kwargs = {
             'tilepath_base': combinedvis_savepath,
@@ -103,25 +132,35 @@ class ReviewGallery(unittest.TestCase):
         # create (+/- post) review gallery
         resps = create_review_galleries(**create_review_galleries_kwargs)
 
+        self.assertEqual(len(resps), 3)
+        self.assertSetEqual(
+            {j['name'] for j in resps},
+            {'gallery-1', 'gallery-2', 'gallery-3'})
+
+        # Now you can go to the histomicstk folder POST_FOLDERID
+        # to check that the visualizations and galler make sense!
+
 # %%===========================================================================
 
-# def suite():
-#     """Run chained unit tests in desired order.
-#
-#     See: https://stackoverflow.com/questions/5387299/...
-#          ... python-unittest-testcase-execution-order
-#     """
-#     suite = unittest.TestSuite()
-#     suite.addTest(TissueDetectionTest('test_get_tissue_mask'))
-#     suite.addTest(
-#         TissueDetectionTest('test_get_tissue_boundary_annotation_documents'))
-#     return suite
-#
-# # %%===========================================================================
-#
-#
-# if __name__ == '__main__':
-#     runner = unittest.TextTestRunner(failfast=True)
-#     runner.run(suite())
-#     # cleanup
-#     shutil.rmtree(savepath)
+
+def suite():
+    """Run chained unit tests in desired order.
+
+    See: https://stackoverflow.com/questions/5387299/...
+         ... python-unittest-testcase-execution-order
+    """
+    suite = unittest.TestSuite()
+    suite.addTest(ReviewGallery('test_get_all_rois_from_folder_v2'))
+    suite.addTest(ReviewGallery('test_create_review_galleries'))
+
+    return suite
+
+# %%===========================================================================
+
+
+if __name__ == '__main__':
+    runner = unittest.TextTestRunner(failfast=True)
+    runner.run(suite())
+
+    # cleanup
+    shutil.rmtree(BASE_SAVEPATH)
