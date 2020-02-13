2edefe8c0b167c1958677a4036e3a6f3a31f1038
kheffah <mtageld@emory.edu>
2020-02-12 22:06:51 -0500

    [WIP] - add convenience functions to update annotation permissions for a whole filder en mass

--- histomicstk/utils/girder_convenience_utils.py
+++ histomicstk/utils/girder_convenience_utils.py
@@ -30,3 +30,102 @@ def get_absolute_girder_folderpath(gc, folder_id=None, folder_info=None):
         [j['object']['name'] for j in fpath]
     ) + "/" + folder_info['name'] + "/"
     return fpath
+
+
+def update_permissions_for_annotation(
+        gc, annotation_id,
+        groups_to_add=[], replace_original_groups=False,
+        users_to_add=[], replace_original_users=False, admin_infos=[]):
+    """Update permissions for a single annotation.
+
+    Parameters
+    ----------
+    gc
+    annotation_id
+    groups_to_add
+    replace_original_groups
+    users_to_add
+    replace_original_users
+    admin_infos
+
+    Returns
+    -------
+
+    """
+    # admins can always edit
+    if replace_original_users:
+        if len(admin_infos) > 0:
+            users_to_add.extend(admin_infos)
+        else:
+            raise Exception("Must provide admin info!")
+
+    # get current permissions
+    current = gc.get('/annotation/%s/access' % annotation_id)
+
+    # add or replace as needed
+
+    if replace_original_groups:
+        current['groups'] = []
+
+    if replace_original_users:
+        current['users'] = []
+
+    for group in groups_to_add:
+        current['groups'].append(group)
+
+    for user in users_to_add:
+        current['users'].append(user)
+
+    # now update accordingly
+    return gc.put('/annotation/%s/access?access=%s' % (
+        annotation_id, json.dumps(current)))
+
+
+def update_permissions_for_annotations_in_slide(
+        gc, slide_id, monitorPrefix='', **kwargs):
+    """Update permissions for all annotations in a slide.
+
+    Parameters
+    ----------
+    gc
+    slide_id
+    monitorPrefix
+
+    Returns
+    -------
+
+    """
+    # get annotations for slide
+    slide_annotations = gc.get('/annotation/item/' + slide_id)
+
+    for ann in slide_annotations:
+        _ = update_permissions_for_annotation(
+            gc=gc, annotation_id=ann['_id'], **kwargs)
+
+
+def update_permissions_for_annotations_in_folder(
+        gc, folderid, workflow_kwargs, monitor=''):
+    """Update permissions for all annotations in a folder.
+
+    Parameters
+    ----------
+    gc
+    folderid
+    workflow_kwargs
+    monitor
+
+    Returns
+    -------
+
+    """
+    workflow_kwargs.update({'gc': gc})
+    workflow_runner = Workflow_runner(
+        slide_iterator=Slide_iterator(
+            gc, source_folder_id=folderid,
+            keep_slides=None,
+        ),
+        workflow=update_permissions_for_annotations_in_slide,
+        workflow_kwargs=workflow_kwargs,
+        monitorPrefix=monitor
+    )
+    workflow_runner.run()
