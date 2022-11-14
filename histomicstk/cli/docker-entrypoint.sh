0c89f6ea0cf8ec25ae1be27cb2cca606af767d21
David Manthey <david.manthey@kitware.com>
2022-11-14 10:51:15 -0500

    Refactor the docker to make it smaller.
    We had been using a cuda docker image, but not using any of its
    features.  The superpixel example
    (https://github.com/DigitalSlideArchive/superpixel-classification) has
    an example that actually uses the GPU.

--- histomicstk/cli/docker-entrypoint.sh
+++ histomicstk/cli/docker-entrypoint.sh
@@ -1,11 +1,5 @@
 #!/usr/bin/env bash
 
-if type nvidia-smi >/dev/null 2>/dev/null;
-then
-    source /venv-gpu/bin/activate
-    echo "NOTE: GPU available" >&2
-fi
-
 # Try to start a local version memcached, but fail gracefully if we can't.
 memcached -u root -d -m 1024 || true
 
