aa9a0e64825ed799969804b4990195a341743318
Subin Erattakulangara <s.erattakulangara@kitware.com>
2023-07-21 10:24:26 -0400

    added return attributes and returnparameters to annotation.

--- histomicstk/cli/BackgroundIntensity/BackgroundIntensity.py
+++ histomicstk/cli/BackgroundIntensity/BackgroundIntensity.py
@@ -1,3 +1,7 @@
+import json
+from pathlib import Path
+
+import histomicstk
 from histomicstk.cli import utils
 from histomicstk.cli.utils import CLIArgumentParser
 from histomicstk.preprocessing.color_normalization import background_intensity
@@ -15,8 +19,26 @@ def main(args):
 
     utils.create_dask_client(args)
     I_0 = background_intensity(**kwargs)
-    with open(args.returnParameterFile, 'w') as f:
-        f.write('BackgroundIntensity = ' + ','.join(map(str, I_0)) + '\n')
+
+    #
+    # Write annotation file
+    #
+
+    print('\n>> Writing annotation file ...\n')
+
+    annotation = {
+        'name': 'Background Intensity',
+        'backgroundIntensity': map(str, I_0),
+        'attributes': {
+            'params': vars(args),
+            'return_params': args.returnParameterFile,
+            'cli': Path(__file__).stem,
+            'version': histomicstk.__version__,
+        },
+    }
+
+    with open(args.returnParameterFile, 'w') as annotation_file:
+        json.dump(annotation, annotation_file, separators=(',', ':'), sort_keys=False)
 
 
 if __name__ == '__main__':
