16a5b7a48881aa990fb6d94485c1001f32cb9a33
Subin Erattakulangara <136746896+subinkitware@users.noreply.github.com>
2023-07-28 11:02:22 -0400

    SeperateStainsMacenkoPCA-added-metadatafile (#1019)

--- histomicstk/cli/SeparateStainsMacenkoPCA/SeparateStainsMacenkoPCA.py
+++ histomicstk/cli/SeparateStainsMacenkoPCA/SeparateStainsMacenkoPCA.py
@@ -1,21 +1,37 @@
+import json
+from pathlib import Path
+
 import numpy
 
+import histomicstk
 from histomicstk.cli import utils
 from histomicstk.cli.utils import CLIArgumentParser
 from histomicstk.preprocessing.color_deconvolution import \
     rgb_separate_stains_macenko_pca
 
 
-def main(args):
-    args = utils.splitArgs(args)
+def main(origargs):
+    args = utils.splitArgs(origargs)
     args.macenko.I_0 = numpy.array(args.macenko.I_0)
 
     utils.create_dask_client(args.dask)
     sample = utils.sample_pixels(args.sample)
     stain_matrix = rgb_separate_stains_macenko_pca(sample.T, **vars(args.macenko))
-    with open(args.returnParameterFile, 'w') as f:
-        for i, stain in enumerate(stain_matrix.T):
-            f.write('stainColor_{} = {}\n'.format(i + 1, ','.join(map(str, stain))))
+
+    # record metadata
+    annotation = {
+        'name': 'SeperateStainsMacenkoPCA',
+        'attributes': {
+            'params': vars(origargs),
+            'cli': Path(__file__).stem,
+            'version': histomicstk.__version__,
+        },
+    }
+    for i, stain in enumerate(stain_matrix.T):
+        annotation['attributes']['stainColor_{}'.format(i + 1)] = stain.tolist()
+
+    with open(args.outputAnnotationFile, 'w') as annotation_file:
+        json.dump(annotation, annotation_file, sort_keys=False)
 
 
 if __name__ == '__main__':
