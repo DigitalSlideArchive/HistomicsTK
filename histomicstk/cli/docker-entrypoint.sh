9fde5678371528504ac6a9e92430bdf2fa5a0a8d
David Manthey <david.manthey@kitware.com>
2024-08-20 08:51:59 -0400

    Speed up listing xml for clis

--- histomicstk/cli/docker-entrypoint.sh
+++ histomicstk/cli/docker-entrypoint.sh
@@ -1,13 +1,19 @@
 #!/usr/bin/env bash
 
-# Try to start a local version memcached, but fail gracefully if we can't.
-memcached -u root -d -m 1024 || true
-
 # Calling the slicer_cli_web.cli_list_entrypoint always works, but we can skip
-# an extra exec if we find the path directly
+# an extra exec if we find the path directly; we can just dump xml if that is
+# requested
 POSSIBLE_PATH="$1/$1.py"
-if [[ -f "$POSSIBLE_PATH" ]]; then
-    python -u "$POSSIBLE_PATH" "${@:2}"
+POSSIBLE_XML_PATH="$1/$1.xml"
+if [[ -f "$POSSIBLE_PATH" && -f "$POSSIBLE_XML_PATH" && "$2" == "--xml" ]]; then
+    cat "$POSSIBLE_XML_PATH"
 else
-    python -u -m slicer_cli_web.cli_list_entrypoint "$@"
+    # Try to start a local version memcached, but fail gracefully if we can't.
+    memcached -u root -d -m 1024 || true
+
+    if [[ -f "$POSSIBLE_PATH" ]]; then
+        python -u "$POSSIBLE_PATH" "${@:2}"
+    else
+        python -u -m slicer_cli_web.cli_list_entrypoint "$@"
+    fi
 fi
