319f477f2468c4929284c6844cd082e157f97a55
David Manthey <david.manthey@kitware.com>
2024-08-23 11:01:24 -0400

    Remove some pre python 3.8 fallback code (#1126)
    Updating some linting

--- histomicstk/__init__.py
+++ histomicstk/__init__.py
@@ -1,15 +1,12 @@
 # import sub-packages to support nested calls
+from importlib.metadata import PackageNotFoundError
+from importlib.metadata import version as _importlib_version
+
 from . import segmentation  # must be imported before features
 from . import utils  # must be imported before other packages
 from . import (annotations_and_masks, features, filters, preprocessing,
                saliency, workflows)
 
-try:
-    from importlib.metadata import PackageNotFoundError
-    from importlib.metadata import version as _importlib_version
-except ImportError:
-    from importlib_metadata import PackageNotFoundError
-    from importlib_metadata import version as _importlib_version
 try:
     __version__ = _importlib_version(__name__)
 except PackageNotFoundError:
