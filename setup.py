2e21f85fbecc049750c935ba925bd153ca685eff
David Manthey <david.manthey@kitware.com>
2024-10-24 12:27:56 -0400

    Build wheels for Python 3.13

--- setup.py
+++ setup.py
@@ -77,6 +77,7 @@ setup(
         'Programming Language :: Python :: 3.10',
         'Programming Language :: Python :: 3.11',
         'Programming Language :: Python :: 3.12',
+        'Programming Language :: Python :: 3.13',
         'Topic :: Scientific/Engineering :: Artificial Intelligence',
         'Topic :: Software Development :: Libraries :: Python Modules',
     ],
