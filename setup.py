9751bec79d024445b83bec677ab05af4a508d35a
David Manthey <david.manthey@kitware.com>
2024-11-13 13:30:24 -0500

    Drop Python 3.8
    It is EOL.

--- setup.py
+++ setup.py
@@ -73,7 +73,6 @@ setup(
         'Development Status :: 5 - Production/Stable',
         'License :: OSI Approved :: Apache Software License',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
         'Programming Language :: Python :: 3.11',
@@ -85,5 +84,5 @@ setup(
     entry_points={
         'console_scripts': ['histomicstk = histomicstk.cli.__main__:main'],
     },
-    python_requires='>=3.8',
+    python_requires='>=3.9',
 )
